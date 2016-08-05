84a3dea8165e2d100a77d206ea6d3244d1fbccdc
melferink <m.elferink@umcutrecht.nl>
2016-08-05 11:25:37 +0200

    Changed IAP_Dx_NIPT to IAP_Dx and added Dx_INI logging

--- cleanup_Dx_NIPT_folder.py
+++ cleanup_Dx_NIPT_folder.py
@@ -7,9 +7,12 @@ import getopt
 os.system(" date >> GIT_IAP.log")
 os.system(" date >> GIT_chromate.log")
 os.system(" date >> GIT_Dx_resources.log")
-os.system("git --git-dir=/hpc/local/CentOS7/cog_bioinf/IAP_Dx_NIPT/.git log > GIT_IAP.log")
+#os.system("git --git-dir=/hpc/local/CentOS7/cog_bioinf/IAP_Dx_NIPT/.git log > GIT_IAP.log")
+os.system("git --git-dir=/hpc/local/CentOS7/cog_bioinf/IAP_Dx/.git log > GIT_IAP.log")
 os.system("git --git-dir=/hpc/local/CentOS7/cog_bioinf/chromate/.git log > GIT_chromate.log")
 os.system("git --git-dir=/hpc/cog_bioinf/data/mapping/diagnostiek/Dx_resources/.git log > GIT_Dx_resources.log")
+os.system("git --git-dir=/hpc/cog_bioinf/data/mapping/diagnostiek/Dx_INI/.git log > GIT_Dx_INI.log")
+
 
 # Make run_stats.txt file from all flagstats
 os.system("/hpc/cog_bioinf/data/mapping/diagnostiek/Dx_resources/get_stats_from_flagstat.pl >run_stats.txt")
