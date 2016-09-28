bd73b6c5741187a1d2254e7ebaaad6bc63fade96
melferink <m.elferink@umcutrecht.nl>
2016-09-28 09:23:45 +0200

    modified to include git version number in log files

--- cleanup_Dx_PANEL_folder.py
+++ cleanup_Dx_PANEL_folder.py
@@ -3,16 +3,42 @@ import sys, os, re
 import commands
 import getopt
 
+# Input folders
+IAP_folder="/hpc/local/CentOS7/cog_bioinf/IAP_Dx/"
+ExonCov_folder="/hpc/local/CentOS7/cog_bioinf/ExonCov/"
+Dx_resources_folder="/hpc/cog_bioinf/data/mapping/diagnostiek/Dx_resources/"
+Dx_INI_folder="/hpc/cog_bioinf/data/mapping/diagnostiek/Dx_INI/"
+# Get GIT tag version
+IAP_v=commands.getoutput("git --git-dir="+str(IAP_folder)+".git describe --tags")
+ExonCov_v=commands.getoutput("git --git-dir="+str(ExonCov_folder)+".git describe --tags")
+Dx_resources_v=commands.getoutput("git --git-dir="+str(Dx_resources_folder)+".git describe --tags")
+Dx_INI_v=commands.getoutput("git --git-dir="+str(Dx_INI_folder)+".git describe --tags")
+# Output files
+output_IAP="GIT_IAP_"+str(IAP_v)+".log"
+output_ExonCov="GIT_ExonCov_"+str(ExonCov_v)+".log"
+output_Dx_resources="GIT_Dx_resources_"+str(Dx_resources_v)+".log"
+output_Dx_INI="GIT_Dx_INI_"+str(Dx_INI_v)+".log"
+
 ## Log git commits of used repo's
-os.system(" date >> GIT_IAP.log")
-os.system(" date >> GIT_ExonCov.log")
-os.system(" date >> GIT_Dx_resources.log")
-os.system(" date >> GIT_Dx_INI.log")
-#os.system("git --git-dir=/hpc/local/CentOS7/cog_bioinf/IAP_Dx_PANEL/.git log > GIT_IAP.log")
-os.system("git --git-dir=/hpc/local/CentOS7/cog_bioinf/IAP_Dx/.git log > GIT_IAP.log")
-os.system("git --git-dir=/hpc/local/CentOS7/cog_bioinf/ExonCov/.git log > GIT_ExonCov.log")
-os.system("git --git-dir=/hpc/cog_bioinf/data/mapping/diagnostiek/Dx_resources/.git log > GIT_Dx_resources.log")
-os.system("git --git-dir=/hpc/cog_bioinf/data/mapping/diagnostiek/Dx_INI/.git log > GIT_Dx_INI.log")
+os.system(" date >> "+str(output_IAP))
+os.system(" date >> "+str(output_ExonCov))
+os.system(" date >> "+str(output_Dx_resources))
+os.system(" date >> "+str(output_Dx_INI))
+## Print used folder in log files
+os.system("echo "+str(IAP_folder)+" >> "+str(output_IAP))
+os.system("echo "+str(ExonCov_folder)+" >> "+str(output_ExonCov))
+os.system("echo "+str(Dx_resources_folder)+" >> "+str(output_Dx_resources))
+os.system("echo "+str(Dx_INI_folder)+" >> "+str(output_Dx_INI))
+## Print GIT version in log files
+os.system("echo "+str(IAP_v)+" >> "+str(output_IAP))
+os.system("echo "+str(ExonCov_v)+" >> "+str(output_ExonCov))
+os.system("echo "+str(Dx_resources_v)+" >> "+str(output_Dx_resources))
+os.system("echo "+str(Dx_INI_v)+" >> "+str(output_Dx_INI))
+## Print full GIT log in log files
+os.system("git --git-dir="+str(IAP_folder)+".git log >> "+str(output_IAP))
+os.system("git --git-dir="+str(ExonCov_folder)+".git log >> "+str(output_ExonCov))
+os.system("git --git-dir="+str(Dx_resources_folder)+".git log >> "+str(output_Dx_resources))
+os.system("git --git-dir="+str(Dx_INI_folder)+".git log >> "+str(output_Dx_INI))
 
 # Make run_stats.txt file from all flagstats
 os.system("/hpc/cog_bioinf/data/mapping/diagnostiek/Dx_resources/get_stats_from_flagstat.pl >run_stats.txt")
@@ -32,8 +58,8 @@ for file in files:
 	os.system(action)
 
 # change dots in Exoncov files into comma's
-action2=" sed -i 's/\./\,/g' "+str(pwd)+"/Exoncov_v3/*tsv"
-action3=" sed -i 's/\./\,/g' "+str(pwd)+"/Exoncov_v3/*/*tsv"
+action2=" sed -i 's/\./\,/g' "+str(pwd)+"/Exoncov_"+str(Dx_resources_v)+"/*tsv"
+action3=" sed -i 's/\./\,/g' "+str(pwd)+"/Exoncov_"+str(Dx_resources_v)+"/*/*tsv"
 os.system(action2)
 os.system(action3)
 
