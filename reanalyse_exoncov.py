f66727fb3fc9bb723d49d168a8a72c6f17446943
melferink <m.elferink@umcutrecht.nl>
2016-10-04 09:46:42 +0200

    revised code to include dynamic and custom output foldername

--- reanalyse_exoncov.py
+++ reanalyse_exoncov.py
@@ -24,9 +24,10 @@ if __name__ == "__main__":
 	group.add_option("-q", default="all.q", dest="EXONCALLCOV_QUEUE", metavar="[STRING]", help="EXONCALLCOV_QUEUE")
 	group.add_option("-k", default="dedup.realigned.bam$", dest="search_string", metavar="[STRING]", help="search_string [default = dedup.realigned.bam$")
 	group.add_option("-m", default="off", dest="EXONCALLCOV_MEM", metavar="[STRING]", help="EXONCALLCOV_MEM")
-	group.add_option("-i", default="OFF", dest="use_ini", action="store_false", help="use information from INI file: OFF [default] or ON")
+	group.add_option("-i", default="off", dest="use_ini", action="store_false", help="use information from INI file: OFF [default] or ON")
         group.add_option("-f", default="/hpc/cog_bioinf/data/mapping/diagnostiek/Dx_resources/", dest="Dx_resources_folder", metavar="[STRING]", help="Dx_resources_folder")
-        parser.add_option_group(group)
+        group.add_option("-o", default="off", dest="use_custom_folder", help="custom output foldername [default=off]")
+	parser.add_option_group(group)
         (opt, args) = parser.parse_args()
 
 	use_ini=opt.use_ini
@@ -64,7 +65,7 @@ if use_ini == "ON":
 			if splitline[0]=="EXONCALLCOV_MEM":
 		                EXONCALLCOV_MEM=splitline[1]
 
-elif use_ini== "OFF":
+elif use_ini== "off":
 	print "INI settings not used"
 	EXONCALLCOV_PATH=opt.EXONCALLCOV_PATH
 	EXONCALLCOV_BED=opt.EXONCALLCOV_BED
@@ -75,17 +76,26 @@ elif use_ini== "OFF":
 	EXONCALLCOV_TIME=opt.EXONCALLCOV_TIME
 	EXONCALLCOV_MEM=opt.EXONCALLCOV_MEM
 
+
+EXONCALLCOV_FOLDER="/".join(EXONCALLCOV_PATH.split("/")[0:-1])  ## EXONCALLCOV_PATH is including script file
+ExonCov_v=commands.getoutput("git --git-dir="+str(EXONCALLCOV_FOLDER)+"/.git describe --tags")
+Dx_resources_v=commands.getoutput("git --git-dir="+str(Dx_resources_folder)+"/.git describe --tags")
+if str(opt.use_custom_folder) == "off":
+	output_folder="Exoncov_reanalysis_GIT_Dx_resources_"+str(Dx_resources_v)
+	output_Dx_resources="Exoncov_reanalysis_GIT_Dx_resources_"+str(Dx_resources_v)+".log"
+	output_ExonCov="Exoncov_reanalysis_GIT_ExonCov_"+str(ExonCov_v)+".log"
+else:	
+	output_folder=str(opt.use_custom_folder)        
+	output_Dx_resources=str(output_folder)+"_GIT_Dx_resources_"+str(Dx_resources_v)+".log"
+	output_ExonCov=str(output_folder)+"_GIT_ExonCov_"+str(ExonCov_v)+".log"
+
+
 ## Perform ExonCov tool
-action="python "+str(EXONCALLCOV_PATH)+ " --queue "+str(EXONCALLCOV_QUEUE)+" -a " +str(EXONCALLCOV_TIME)+ " -c "+ str(EXONCALLCOV_MEM) +" -b "+str(EXONCALLCOV_BED)+" -n "+str(EXONCALLCOV_ENS)+" -p "+ str(EXONCALLCOV_PREF)+ " -l "+str(EXONCALLCOV_PANEL)+ " -d "+ str(search_string) 
+action="python "+str(EXONCALLCOV_PATH)+ " --queue "+str(EXONCALLCOV_QUEUE)+" -a " +str(EXONCALLCOV_TIME)+ " -c "+ str(EXONCALLCOV_MEM) +" -b "+str(EXONCALLCOV_BED)+" -n "+str(EXONCALLCOV_ENS)+" -p "+ str(EXONCALLCOV_PREF)+ " -l "+str(EXONCALLCOV_PANEL)+ " -d "+ str(search_string) + " -o " +str(output_folder)
 print action
 os.system(action)
 
 ## Write Git-log files
-EXONCALLCOV_FOLDER="/".join(EXONCALLCOV_PATH.split("/")[0:-1])	## EXONCALLCOV_PATH is including script file
-ExonCov_v=commands.getoutput("git --git-dir="+str(EXONCALLCOV_FOLDER)+"/.git describe --tags")
-Dx_resources_v=commands.getoutput("git --git-dir="+str(Dx_resources_folder)+"/.git describe --tags")
-output_ExonCov="Exoncov_reanalysis_GIT_ExonCov_"+str(ExonCov_v)+".log"
-output_Dx_resources="Exoncov_reanalysis_GIT_Dx_resources_"+str(Dx_resources_v)+".log"
 os.system(" date >> "+str(output_ExonCov))
 os.system(" date >> "+str(output_Dx_resources))
 os.system("echo "+str(ExonCov_v)+" >> "+str(output_ExonCov))
@@ -94,10 +104,17 @@ os.system("echo "+str(ExonCov_v)+" >> "+str(output_ExonCov))
 os.system("echo "+str(Dx_resources_v)+" >> "+str(output_Dx_resources))
 os.system("git --git-dir="+str(EXONCALLCOV_FOLDER)+"/.git log >> "+str(output_ExonCov))
 os.system("git --git-dir="+str(Dx_resources_folder)+"/.git log >> "+str(output_Dx_resources))
-action2=" sed -i 's/\./\,/g' "+"Exoncov_"+str(Dx_resources_v)+"/*tsv"
-action3=" sed -i 's/\./\,/g' "+"Exoncov_"+str(Dx_resources_v)+"/*/*tsv"
+
+
+if str(opt.use_custom_folder) == "off": 
+	action2= "mv Exoncov_reanalysis_GIT*.log "+str(output_folder)
+else:
+	action2= "mv "+str(output_folder)+"_GIT*.log "+str(output_folder)
+action3=" sed -i 's/\./\,/g' "+str(output_folder)+"/*tsv"
+action4=" sed -i 's/\./\,/g' "+str(output_folder)+"/*/*tsv"
 os.system(action2)
 os.system(action3)
+os.system(action4)
 
 print "finished"
 
