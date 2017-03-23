719484ea9266a098c63f22ce243b4028e19ed148
melferink <m.elferink@umcutrecht.nl>
2017-03-23 09:26:22 +0100

    first commit

---
+++ FINGERPRINT/allelefreqhet_mips.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Simple script to determine average ALT allelefrequency in 0/1 calls per MIP in current folder
+# Would like to add chromosome numbers (column 1 in 81_snps_mip_design_nijmegen_sort.vcf) to output and fix error when MIP has 0 coverage
+
+a=`cat /hpc/cog_bioinf/diagnostiek/production/Dx_resources/FINGERPRINT/81_snps_mip_design_nijmegen_sort.vcf | grep -v '#' | cut -f2`
+
+for SNP in $a; do
+	if [[ $SNP != '2847439' && $SNP != '2847910' ]] 
+                then
+                        printf "$SNP\t"
+			b=`cat *vcf | grep $SNP | grep 0/1 | cut -f10 | sed 's/:/\t/g' | sed 's/,/\t/g' | cut -f3 | awk '{sum+=$1} END {print sum}'`
+			c=`cat *vcf | grep $SNP | grep 0/1 | cut -f10 | sed 's/:/\t/g' | sed 's/,/\t/g' | cut -f4 | awk '{sum+=$1} END {print sum}'`
+			d=`echo $b/$c|bc -l`
+			echo $d
+	fi
+done
