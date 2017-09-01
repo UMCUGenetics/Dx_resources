5c0286cc330e091533d92acf2f618411720398c5
bkant <bkant@hpcs04.op.umcutrecht.nl>
2017-09-01 13:56:21 +0200

    MIPS cleanup further automated

--- cleanup_Dx_MIPS_folder.py
+++ cleanup_Dx_MIPS_folder.py
@@ -1,71 +1,53 @@
 #!/usr/bin/env python
 
+import commands
 import fnmatch
+import glob
 import os
 import shutil
-import commands
 
-# Versioning input folders
-IAP_folder="/hpc/local/CentOS7/cog_bioinf/IAP_Dx/"
-Dx_resources_folder="/hpc/cog_bioinf/diagnostiek/production/Dx_resources/"
-Dx_INI_folder="/hpc/cog_bioinf/diagnostiek/production/Dx_INI/"
-Dx_tracks_folder="/hpc/cog_bioinf/diagnostiek/production/Dx_tracks/"
-Dx_mips_folder="/hpc/cog_bioinf/diagnostiek/production/Dx_mips/"
-Dx_fingerprinting_folder="/hpc/cog_bioinf/diagnostiek/production/Dx_fingerprinting/"
-# Get GIT tag version
-IAP_v=commands.getoutput("git --git-dir="+str(IAP_folder)+".git describe --tags")
-Dx_resources_v=commands.getoutput("git --git-dir="+str(Dx_resources_folder)+".git describe --tags")
-Dx_INI_v=commands.getoutput("git --git-dir="+str(Dx_INI_folder)+".git describe --tags")
-Dx_tracks_v=commands.getoutput("git --git-dir="+str(Dx_tracks_folder)+".git describe --tags")
-Dx_mips_v=commands.getoutput("git --git-dir="+str(Dx_mips_folder)+".git describe --tags")
-Dx_fingerprinting_v=commands.getoutput("git --git-dir="+str(Dx_fingerprinting_folder)+".git describe --tags")
-# Versioning output files
-output_IAP="GIT_IAP_"+str(IAP_v)+".log"
-output_Dx_resources="GIT_Dx_resources_"+str(Dx_resources_v)+".log"
-output_Dx_INI="GIT_Dx_INI_"+str(Dx_INI_v)+".log"
-output_Dx_tracks="GIT_Dx_tracks_"+str(Dx_tracks_v)+".log"
-output_Dx_mips="GIT_Dx_mips_"+str(Dx_mips_v)+".log"
-output_Dx_fingerprinting="GIT_Dx_fingerprinting_"+str(Dx_fingerprinting_v)+".log"
+# Functions
 
-print "Making GIT log files\n"
+""" Create git logfiles for files in inputlist """ 
+def logging(inputlist):
+    for repo in inputlist:
+        index = inputlist.index(repo)
+        repo_folder = folderlist[index]
+        repo_v = commands.getoutput("git --git-dir={}.git describe --tags".format(repo_folder))
+        output_repo = "GIT_{}_{}.log".format(repo, repo_v)
+        os.system(" date >> "+str(output_repo))
+        os.system("echo "+str(repo_folder)+" >> "+str(output_repo))
+        os.system("echo "+str(repo_v)+" >> "+str(output_repo))
+        os.system("git --git-dir="+str(repo_folder)+".git log >> "+str(output_repo))
 
-## Log git commits of used repo's
-os.system(" date >> "+str(output_IAP))
-os.system(" date >> "+str(output_Dx_resources))
-os.system(" date >> "+str(output_Dx_INI))
-os.system(" date >> "+str(output_Dx_tracks))
-os.system(" date >> "+str(output_Dx_mips))
-os.system(" date >> "+str(output_Dx_fingerprinting))
-## Print used folder in log files
-os.system("echo "+str(IAP_folder)+" >> "+str(output_IAP))
-os.system("echo "+str(Dx_resources_folder)+" >> "+str(output_Dx_resources))
-os.system("echo "+str(Dx_INI_folder)+" >> "+str(output_Dx_INI))
-os.system("echo "+str(Dx_tracks_folder)+" >> "+str(output_Dx_tracks))
-os.system("echo "+str(Dx_mips_folder)+" >> "+str(output_Dx_mips))
-os.system("echo "+str(Dx_fingerprinting_folder)+" >> "+str(output_Dx_fingerprinting))
-## Print GIT version in log files
-os.system("echo "+str(IAP_v)+" >> "+str(output_IAP))
-os.system("echo "+str(Dx_resources_v)+" >> "+str(output_Dx_resources))
-os.system("echo "+str(Dx_INI_v)+" >> "+str(output_Dx_INI))
-os.system("echo "+str(Dx_tracks_v)+" >> "+str(output_Dx_tracks))
-os.system("echo "+str(Dx_mips_v)+" >> "+str(output_Dx_mips))
-os.system("echo "+str(Dx_fingerprinting_v)+" >> "+str(output_Dx_fingerprinting))
-## Print full GIT log in log files
-os.system("git --git-dir="+str(IAP_folder)+".git log >> "+str(output_IAP))
-os.system("git --git-dir="+str(Dx_resources_folder)+".git log >> "+str(output_Dx_resources))
-os.system("git --git-dir="+str(Dx_INI_folder)+".git log >> "+str(output_Dx_INI))
-os.system("git --git-dir="+str(Dx_tracks_folder)+".git log >> "+str(output_Dx_tracks))
-os.system("git --git-dir="+str(Dx_mips_folder)+".git log >> "+str(output_Dx_mips))
-os.system("git --git-dir="+str(Dx_fingerprinting_folder)+".git log >> "+str(output_Dx_fingerprinting))
+""" Move disapproved samples to bad data folder """
+def move(vcf):
+    for i in os.listdir('.'):
+        if i.startswith(vcf):
+            shutil.move(i, badfolder)
 
-print "Check results:"
+# Variables
 
-# Result folder and files
+inputlist = ["IAP", "Dx_resources", "Dx_INI", "Dx_tracks", "Dx_mips", "Dx_fingerprinting"]
+folderlist = ["/hpc/local/CentOS7/cog_bioinf/IAP_Dx/", 
+              "/hpc/cog_bioinf/diagnostiek/production/Dx_resources/",
+              "/hpc/cog_bioinf/diagnostiek/production/Dx_INI/",
+              "/hpc/cog_bioinf/diagnostiek/production/Dx_tracks/",
+              "/hpc/cog_bioinf/diagnostiek/production/Dx_mips/",
+              "/hpc/cog_bioinf/diagnostiek/production/Dx_fingerprinting/"]
+
 badfolder = 'disapprovedVCFs'
-preoutput = '../tmp.txt'
-output = '../logbook.txt'
 
-## Generate results
+## Create GIT log files
+
+print "\nMaking GIT log files..."
+
+logging(inputlist)
+
+## Check results
+
+print "\nCheck results..."
+
 os.chdir('./fingerprint')
 os.mkdir(badfolder)
 for file in os.listdir('.'):
@@ -76,6 +58,7 @@ for file in os.listdir('.'):
             hom_count = 0
             y_reads = 0 
             low_cov = 0
+            false_het = 0
             for line in f:
                 if not line.startswith('#'):
                     line = line.split()
@@ -101,39 +84,61 @@ for file in os.listdir('.'):
                         else:
                             if int(values[3]) < 15:
                                 low_cov += 1
+                    if values[0] == '0/1':
+                        af_value = int(values[1]) / float(int(values[3]))
+                        if af_value > 0.8 or af_value < 0.2:
+                            false_het += 1             
+
             contamination = column2 / float(column4)
+
             result = file, str(low_cov), str(hom_count), str(round(contamination,6)), file[8], str(y_reads)            
-
-### Print warnings and replace files
-            if result[4] != 'M' and result[4] != 'F':
-                print('### For ' + file + ', report filename issue (no M/F) to lab')
-            if int(result[1]) > 4: 
-                print('### ' + file + ' has ' + str(low_cov) + ' SNPs with coverage <15X --> disapproved!')
-                for sample in os.listdir('.'):
-                    if sample.startswith(file):
-                        shutil.move(sample, badfolder)                
-	    elif float(result[3]) > 0.02:
-                print('### ' + file + ' has a contamination value of ' + str("{:.2%}".format(contamination)) + ', disapproved!')
-                for sample in os.listdir('.'):
-                    if sample.startswith(file):
-                        shutil.move(sample, badfolder)
-            elif int(result[2]) < 10:
-                print('### ' + file + ' has ' + str(hom_count) + ' SNPs called homozygous, check VCF for contamination!')
-            elif result[4] == 'M' and int(result[5]) < 30:
-                print('### ' + file + ' has ' + str(y_reads) + ' reads on chromosome Y, if no low coverage message, check VCF!')
-            elif result[4] == 'F' and int(result[5]) > 30:
-                print('### ' + file + ' has ' + str(y_reads) + ' reads on chromosome Y, if no contamination message, check VCF!')
-
-#### Create logbook
             str_result = '\t'.join(list(result))
-            os.system("echo " + str_result + " >> " + preoutput)
+            os.system("echo " + str_result + " >> ../logs.tmp")
 
-##### Clean-up logbook
-f = open(output,'w')
-f.write('\n')
-f.write('\n')
-f.close()
-os.system("cat " + preoutput + " | sort | sed 's/ /\t/g' >> " + output)
-os.remove(preoutput)
+            with open('../message.tmp','a') as message:
+                gender = ['M', 'F', 'O']
+                if result[4] not in gender:
+                    message.write('### {}: REPORT filename issue TO LAB'.format(file) + '\n')
+                if int(result[1]) > 4: 
+                    message.write('### {}: {} SNPs with <15X coverage --> disapproved'.format(file, result[1]) + '\n') 
+                    move(file)                
+                elif float(result[3]) > 0.02:
+                    message.write('### {}: Contamination value {:.2%} --> disapproved'.format(file, contamination) + '\n')
+                    move(file)
+                elif int(result[2]) < 10:
+                    if false_het > 3: 
+                        message.write('### {}: Only {} homozygous SNPs called, {} unbalanced heterozygous calls --> disapproved!'.format(file, hom_count, false_het) + '\n')
+                        move(file)
+                    else:
+                        message.write('### {}: Only {} homozygous SNPs called but no contamination detected --> OK'.format(file, hom_count) + '\n')
+                elif result[4] == 'F' and int(result[5]) > 30:
+                    if false_het > 3: 
+                        message.write('### {}: {} reads on chromosome Y, {} unbalanced heterozygous calls --> disapproved!'.format(file, y_reads) + '\n')
+                        move(file)
+                    else:
+                        message.write('### {}: {} reads on chromosome Y but no contamination detected, REPORT TO LAB'.format(file, y_reads) + '\n')
+                if result[4] == 'M' and int(result[5]) < 100:
+                    message.write('### {}: Only {} reads on chromosome Y, REPORT TO LAB'.format(file, y_reads) + '\n')
+                
+## Write logbook
 
-print('\nFinished!')
+print('\nCreating logbook...')
+
+os.chdir('..')
+os.system("cat logs.tmp | sort | sed 's/ /\t/g' > logbook.tmp")
+os.system("cat message.tmp | sort > messagelog.tmp")
+with open('logbook.txt','a') as out:
+    with open('messagelog.tmp','r') as message, open('logbook.tmp','r') as logs:
+        out.write('\n')
+        for mline in message:
+            mline = mline.rstrip()
+            out.write(mline + '\n')
+        out.write('\n')
+        for lline in logs:
+            lline = lline.rstrip()
+            out.write(lline + '\n')
+
+for tmp in glob.glob("*.tmp"):
+    os.remove(tmp)
+
+print('\nFinished!\n')
