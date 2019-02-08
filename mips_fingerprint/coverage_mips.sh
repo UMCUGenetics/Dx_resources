cc1927f91d71f5fb8601d7270ac64f7ba565a874
rernst <r.f.ernst-3@umcutrecht.nl>
2019-02-08 14:55:52 +0100

    Use genotype DP

--- mips_fingerprint/coverage_mips.sh
+++ mips_fingerprint/coverage_mips.sh
@@ -8,15 +8,14 @@ snp_positions=`cat $design_vcf | grep -v '#' | cut -f2`
 samples=`ls -1 *.vcf | wc -l`
 
 for snp in $snp_positions; do
+	printf "$snp\t"
 	if [[ $snp != '2847439' && $snp != '2847910' ]]
 		then
-			printf "$snp\t"
-            dp_sum=`cat *.vcf | $SnpSift filter POS=$snp | $SnpSift extractFields - DP | awk '{sum+=$1} END {print sum}'`
-            dp_avg=`expr $dp_sum / $samples`
+			dp_sum=`cat *.vcf | $SnpSift filter POS=$snp | $SnpSift extractFields - GEN[0].DP | awk '{sum+=$1} END {print sum}'`
+			dp_avg=`expr $dp_sum / $samples`
 			echo $dp_avg
 		else
-			printf "$SNP\t"
-            dp_avg=`cat *.vcf | $SnpSift filter POS=$snp | $SnpSift filter 'isRef(GEN[0])' | $SnpSift extractFields - DP | grep -v "#" | awk '{sum+=$1; if ($1) n++; } END {print sum / n}'`
+			dp_avg=`cat *.vcf | $SnpSift filter POS=$snp | $SnpSift filter 'isRef(GEN[0])' | $SnpSift extractFields - GEN[0].DP | grep -v "#" | awk '{sum+=$1; if ($1) n++; } END {print sum / n}'`
 			echo $dp_avg
 	fi
 done
