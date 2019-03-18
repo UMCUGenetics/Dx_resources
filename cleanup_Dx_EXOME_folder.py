62dda35a5590bdabd81224eb4eec79ef508e60dc
Nienke van Unen <45174721+nienkevanunen@users.noreply.github.com>
2019-03-18 09:59:57 +0100

    Update cleanup_Dx_EXOME_folder.py

--- cleanup_Dx_EXOME_folder.py
+++ cleanup_Dx_EXOME_folder.py
@@ -21,7 +21,7 @@ def pedfile_dic(pwd):
     family = {}
     for line in lines:
         splitline = line.split()
-	if (splitline):
+	if splitline:
             try:
                 family[splitline[1]]
 	    except:
