d8fddb25533a473f48d949838248f6bbebb55b9e
rernst <r.f.ernst-3@umcutrecht.nl>
2019-04-11 15:03:19 +0200

    Round Y snps and add '#' filter

--- mips_fingerprint/coverage_mips.sh
+++ mips_fingerprint/coverage_mips.sh
@@ -11,11 +11,11 @@ for snp in $snp_positions; do
 	printf "$snp\t"
 	if [[ $snp != '2847439' && $snp != '2847910' ]]
 		then
-			dp_sum=`cat *.vcf | $SnpSift filter POS=$snp | $SnpSift extractFields - GEN[0].DP | awk '{sum+=$1} END {print sum}'`
+			dp_sum=`cat *.vcf | $SnpSift filter POS=$snp | $SnpSift extractFields - GEN[0].DP | grep -v "#" | awk '{sum+=$1} END {print sum}'`
 			dp_avg=`expr $dp_sum / $samples`
 			echo $dp_avg
 		else
-			dp_avg=`cat *.vcf | $SnpSift filter POS=$snp | $SnpSift filter 'isRef(GEN[0])' | $SnpSift extractFields - GEN[0].DP | grep -v "#" | awk '{sum+=$1; if ($1) n++; } END {print sum / n}'`
+			dp_avg=`cat *.vcf | $SnpSift filter POS=$snp | $SnpSift filter 'isRef(GEN[0])' | $SnpSift extractFields - GEN[0].DP | grep -v "#" | awk '{sum+=$1; if ($1) n++; } END { printf("%3.0f", sum / n) }'`
 			echo $dp_avg
 	fi
 done
