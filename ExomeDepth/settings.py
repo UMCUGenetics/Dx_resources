3670ef42032fe681ffa8c0faab2c923423af37c3
melferink <m.elferink@umcutrecht.nl>
2020-03-02 08:05:48 +0100

    update to diaggen folder

--- ExomeDepth/settings.py
+++ ExomeDepth/settings.py
@@ -1,7 +1,7 @@
 ## Settings used in run_Exomdepth.py ##
 
 # Location of repository
-cwd = "/hpc/cog_bioinf/diagnostiek/development/Dx_resources_ED/ExomeDepth/"
+cwd = "/hpc/diaggen/software/development/Dx_resources_ED/ExomeDepth/"
 
 # R script for RefSet and CNV calling
 callcnv_r=str(cwd)+"Exomedepth_callCNVs.R"
@@ -12,29 +12,30 @@ csv2vcf= str(cwd)+"ed_csv_to_vcf.py"
 template=str(cwd)+"template.vcf"
 
 # Reference genome
-reference_genome= "/hpc/cog_bioinf/GENOMES/Homo_sapiens.GRCh37.GATK.illumina/Homo_sapiens.GRCh37.GATK.illumina.fasta"
+reference_genome= "/hpc/diaggen/data/databases/ref_genomes/Homo_sapiens.GRCh37.GATK.illumina/Homo_sapiens.GRCh37.GATK.illumina.fasta"
 
 #Used R version
 Rversion="R/3.5.1"
 
 # Location of target files
-refset_dir="/hpc/cog_bioinf/diagnostiek/development/Dx_tracks/ExomeDepth/"
+refset_dir="/hpc/diaggen/data/databases/ExomeDepth_refset/"
+reffile_dir="/hpc/diaggen/software/development/Dx_tracks_ED/ExomeDepth/"
 
 #Reference set
-#refset="Nov2019"
-refset="Run0280"
+refset="Nov2019"
+
 analysis={"HC":
               {"refset":{"female":str(refset_dir)+"/HC_female_"+str(refset)+".EDref",
                          "male":str(refset_dir)+"/HC_male_"+str(refset)+".EDref"},
-               "target_bed":str(refset_dir)+"High_confident_SureSelect_CREv2_elidS30409818_Covered_dp30_500_cv20.bed",
-               "exon_bed":str(refset_dir)+"exons.hg19.full_HC_CREv2_elidS30409818.tsv"
+               "target_bed":str(reffile_dir)+"High_confident_SureSelect_CREv2_elidS30409818_Covered_dp30_500_cv20.bed",
+               "exon_bed":str(reffile_dir)+"exons.hg19.full_HC_CREv2_elidS30409818.tsv"
                },
 
           "UMCU": 
                {"refset":{"female":str(refset_dir)+"/UMCU_female_"+str(refset)+".EDref",
                           "male":str(refset_dir)+"/UMCU_male_"+str(refset)+".EDref"},
-               "target_bed":str(refset_dir)+"ENSEMBL_UCSC_merged_collapsed_sorted_v3_20bpflank_flat.bed",
-               "exon_bed":str(refset_dir)+"exons.hg19.full_UMCU20bp.tsv"
+               "target_bed":str(reffile_dir)+"ENSEMBL_UCSC_merged_collapsed_sorted_v3_20bpflank_flat.bed",
+               "exon_bed":str(reffile_dir)+"exons.hg19.full_UMCU20bp.tsv"
                }
          }   
 
