58d9bb92a00f5ac5aefaf8b75cb2faadda4816a7
melferink <m.elferink@umcutrecht.nl>
2020-03-03 16:10:30 +0100

    refactor

--- ExomeDepth/run_ExomeDepth.py
+++ ExomeDepth/run_ExomeDepth.py
@@ -7,11 +7,9 @@ import re
 from optparse import OptionParser, OptionGroup
 import settings
 
-
 def valid_read(read):
     """Check if a read is properly mapped."""
-    if (read.mapping_quality >= 20 and
-            read.reference_end and read.reference_start is not None):
+    if (read.mapping_quality >= 20 and read.reference_end and read.reference_start):
         return True
     else:
         return False
@@ -21,11 +19,7 @@ def get_gender(bam):
     """Determine chrY ratio based on read count in bam (excl PAR)."""
     workfile = pysam.AlignmentFile(bam, "rb")
     locus = 'Y:2649520-59034050'
-    yreads = float(sum([valid_read(read)
-                        for read in workfile.fetch(region=locus)
-                        ]
-                       )
-                   )
+    yreads = float(sum([valid_read(read) for read in workfile.fetch(region=locus)]))
     total = float(workfile.mapped)
     yratio = float("%.2f" % ((yreads / total) * 100))
     if yratio <= 0.06:
@@ -84,21 +78,21 @@ else:
     prefix = str(wkdir).split("/")[-1]
 
 if opt.gender_file:
-    g_dic = {}
-    g_file = open(str(opt.gender_file), "r")
-    for line in g_file:
+    gender_dic = {}
+    gender_file = open(str(opt.gender_file), "r")
+    for line in gender_file:
         splitline = line.split()
-        if splitline[0] not in g_dic:
-            g_dic[splitline[0]] = splitline[1]
+        if splitline[0] not in gender_dic:
+            gender_dic[splitline[0]] = splitline[1]
+    gender_file.close()
 
 analysis = settings.analysis
-ed_r = settings.callcnv_r
+ed_r = settings.call_cnv_r
 refgenome = settings.reference_genome
 qsub_call = str(settings.qsub_call) + str(opt.mail)
 qsub_ref = str(settings.qsub_ref) + str(opt.mail)
 prob = settings.probability
 gender = settings.gender
-
 
 """Log all settings in setting.log file"""
 log_dir=str(outdir)+"/logs"
@@ -106,21 +100,17 @@ os.system("mkdir -p "+str(log_dir))
 if opt.input_folder:
     write_file=open(log_dir+"/settings.log","w")
 elif opt.input_bam:
-     write_file=open(log_dir+"/"+str(opt.input_bam.split("/")[-1])+"_settings.log","w")
+    write_file=open("{0}/{1}__settings.log".format(log_dir+str(opt.input_bam.split("/")[-1])),"w")
 (options, args) = parser.parse_args()
 for item in vars(options):
-    write_file.write(str(item)
-                     + "\t"
-                     + str(vars(options)[item])
-                     + "\n"
+    write_file.write(str(item) + "\t" + str(vars(options)[item]) + "\n"
                      )
 for item in dir(settings):
     if "__" not in item:
-        write_file.write(str(item)
-                         + "\t"
-                         + str(repr(eval("settings.%s" % item)))
-                         + "\n"
-                         )
+        write_file.write("{0} {1} \n".format(
+                         item,
+                         str(repr(eval("settings.%s" % item)))
+                         ))
 write_file.close()
 
 if opt.make_ref and not opt.make_call:
@@ -129,14 +119,11 @@ if opt.make_ref and not opt.make_call:
     if opt.input_bam:
         sys.exit("please provide BAM folder")
 
-    bams = commands.getoutput("find -L "
-                              + str(wkdir)
-                              + " -iname \"*realigned.bam\" "
-                              ).split()
-    print "Number of BAM files detected = " + str(len(bams))
+    bams =  commands.getoutput("find -L {0} -iname \"*realigned.bam\" ".format(wkdir)).split()
+    print("Number of BAM files detected = {0}".format(len(bams)))
 
     """Get gender from chrY read count ratio."""
-    dic = {}
+    dic = {}  #Dictionary with gender of each sample
     for item in gender:
         if str(item) not in dic:
             dic[str(item)] = []
@@ -146,37 +133,31 @@ if opt.make_ref and not opt.make_call:
         if gender is not "unknown":
             dic[get_gender(bam)] += [bam]
         else:
-            print ("Sample "
-                   + str(bam)
-                   + "  has unknown gender and is removed from analysis"
-                   )
+            print("Sample {0} has unknown gender and is removed from analysis".format(bam))
 
     """Make folder per gender + analysis, and soflink BAMs in these folders."""
     for target in analysis:
         for item in dic:
-            folder = (str(outdir) + "/" + str(target) + "_"+str(item) + "_"
-                      + str(wkdir).split("/")[-1]
-                      )
-            output_id = (str(target) + "_" + str(item) + "_"
-                         + str(prefix) + ".EDref"
-                         )
-            os.system("mkdir -p " + str(folder))
+            folder = "{0}/{1}_{2}_{3}".format(outdir,target,item,str(wkdir).split("/")[-1])  
+            output_id = "{0}_{1}_{2}.EDref".format(target,item,prefix)
+            os.system("mkdir -p {0}".format(folder))
             for bam in dic[item]:
                 os.system("ln -sd " + str(bam) + "* " + str(folder))
             write_file = open(str(folder) + "/make_ref.sh", "w")
             write_file.write(str(qsub_ref) + "\n")
-            write_file.write("module load " + str(settings.Rversion)+"\n")
-            write_file.write("cd " + str(folder) + "\n")
-            write_file.write("Rscript " + str(settings.createrefset_r) + " "
-                             + str(folder) + "/ " + str(folder) + "/"
-                             + str(output_id) + " "
-                             + str(analysis[target]["target_bed"]) + " "
-                             + str(settings.reference_genome) + " "
-                             + str(analysis[target]["exon_bed"]) + "\n"
-                             )
+            write_file.write("module load {0}\n".format(settings.r_version))
+            write_file.write("cd {0}\n".format(folder))
+            write_file.write("Rscript {0} {1}/{1}/{2} {3} {4} {5}\n".format(
+                             settings.create_refset_r,
+                             folder,
+                             output_id,
+                             analysis[target]["target_bed"],
+                             settings.reference_genome,
+                             analysis[target]["exon_bed"]
+                             ))
             write_file.close()
             os.chdir(str(folder))
-            os.system("qsub " + str(folder) + "/make_ref.sh")
+            os.system("qsub {0}/make_ref.sh".format(folder))
 
 elif opt.make_call and not opt.make_ref:  # Call CNV from BAMs
     """Make CNV call on sample(s)."""
@@ -184,11 +165,8 @@ elif opt.make_call and not opt.make_ref:  # Call CNV from BAMs
     if opt.input_bam:
         bams=[str(opt.input_bam)]
     else:
-        bams= commands.getoutput("find -L " + str(wkdir) 
-                                 + " -iname \"*realigned.bam\" "
-                                 ).split()
-
-    print "Number of BAM files detected = " + str(len(bams))
+        bams= commands.getoutput("find -L {0} -iname \"*realigned.bam\"".format(wkdir)).split()
+    print("Number of BAM files detected = {0}".format(len(bams)))
     if not bams:
         sys.exit("no bams detected")
 
@@ -196,94 +174,112 @@ elif opt.make_call and not opt.make_ref:  # Call CNV from BAMs
         """Get gender from chrY read count ratio."""
         gender = get_gender(bam)
         if opt.gender_file:  # overrule gender as given in gender_file
-            for item in g_dic:
+            for item in gender_dic:
                 if str(item) in str(bam):
-                    gender = g_dic[item]
+                    gender = gender_dic[item]
         if gender == "unknown":  # try to determine gender on BAM ID annotation
             if re.search('[C|P]M', bam.split("/")[-1]):
                 gender = "male"
             elif re.search('[C|P]F', bam.split("/")[-1]):
                 gender = "female"
             else:
-                print ("Sample " + str(bam.split("/")[-1])
-                       + " has a unknown gender and will not be analysed"
-                       )
+                print("Sample {0} has a unknown gender and will not be analysed".format(bam.split("/")[-1]))
                 continue
 
         for item in analysis:
-            print "submitting " + str(item) + " jobs"
+            print("Submitting {0} jobs".format(item))
             sampleid = bam.split("/")[-1].split("_")[0]
-            outfolder = (str(outdir) + "/"+ str(item)+"/" + str(item) + "_"
-                         + str(bam.split("/")[-1])
-                         )
-            os.system("mkdir -p " + str(outfolder))
+            outfolder = "{0}/{1}/{1}_{2}".format(outdir,item,bam.split("/")[-1])
+            os.system("mkdir -p {0}".format(outfolder))
             if opt.input_bam:  #Single BAM processing in serial
                 os.chdir(outfolder)
-                os.system("module load " + str(settings.Rversion) + "&& Rscript " 
-                          + str(ed_r) + " " + str(analysis[item]["refset"][gender]) 
-                          + " " + str(analysis[item]["target_bed"]) + " " 
-                          + str(refgenome) + " " + str(analysis[item]["exon_bed"]) 
-                          + " " + str(prob[str(item)]) + " "+str(bam)
-                          )
-                os.system("rename " + str(bam.split("/")[-1]) + " " 
-                          + str(item)+"_" + str(settings.refset)
-                          + "_" + str(bam.split("/")[-1]) + " *"
-                          )
-                os.system("python " + str(settings.csv2vcf) + " -i " + str(outfolder) 
-                          + " -t " + str(settings.template) + " -m " 
-                          + str(analysis[item]["refset"][gender]) 
-                          + " --id "+ str(sampleid)
-                          )
-                os.system("cp " + str(outfolder) + "/*vcf " + str(outdir) + "/" + str(item))
-
+                os.system("module load {0} && Rscript {1} {2} {3} {4} {5} {6} {7}".format(
+                          settings.r_version,
+                          ed_r,
+                          analysis[item]["refset"][gender],
+                          analysis[item]["target_bed"],
+                          refgenome,
+                          analysis[item]["exon_bed"],
+                          str(prob[str(item)]),
+                          bam
+                         ))
+                os.system("rename {0} {1}_{2}_{3} *".format(
+                          bam.split("/")[-1],
+                          item,
+                          settings.refset,
+                          bam.split("/")[-1]
+                          ))
+                os.system("python {0} -i {1} -t {2} -m {3} --id {4}".format(
+                          settings.csv2vcf,
+                          outfolder,
+                          settings.template,
+                          analysis[item]["refset"][gender],
+                          sampleid
+                         ))
+                os.system("cp {0}/*vcf {1}/{2}".format(
+                          outfolder,
+                          outdir,
+                          item
+                         ))
             else:  # multiple BAM processing for all BAMs in folder (SGE)
-                write_file = open(str(outfolder) + "/" + str(item)
-                                  + "_" + str(gender) + "_"
-                                  + bam.split("/")[-1][:-4] + ".sh", "w"	
-                                  )   #[:-4] easier for renaming before csv2vcf					
+                write_file=open("{0}/{1}_{2}_{3}.sh".format(
+                                outfolder,
+                                item,
+                                gender,
+                                bam.split("/")[-1][:-4]		#[:-4] easier for renaming before csv2vcf
+                                ), "w")  
                 write_file.write(str(qsub_call) + "\n")
-                write_file.write("module load " + str(settings.Rversion) + "\n")
-                write_file.write("cd " + str(outfolder) + "\n")
-                write_file.write("Rscript " + str(ed_r) + " "
-                                 + str(analysis[item]["refset"][gender])
-                                 + " " + str(analysis[item]["target_bed"])
-                                 + " " + str(refgenome) + " "
-                                 + str(analysis[item]["exon_bed"])
-                                 + " " + str(prob[str(item)]) 
-                                 + " " + str(bam) + "\n"
-                                 )
+                write_file.write("module load {0}\n".format(settings.r_version))
+                write_file.write("cd {0}\n".format(outfolder))
+                write_file.write("Rscript {0} {1} {2} {3} {4} {5} {6}\n".format(
+                                 ed_r,
+                                 analysis[item]["refset"][gender],
+                                 analysis[item]["target_bed"],
+                                 refgenome,
+                                 analysis[item]["exon_bed"],
+                                 str(prob[str(item)]),
+                                 bam
+                                 ))
                 write_file.close()
                 """Submit jobs."""
                 os.chdir(outfolder)
-                command = ("qsub " + str(outfolder) + "/"
-                           + str(item) + "_" + str(gender)
-                           + "_" + bam.split("/")[-1][:-4] + ".sh"
-                           )
+                command="qsub {0}/{1}_{2}_{3}.sh ".format(
+                         outfolder,
+                         item,
+                         gender,
+                         bam.split("/")[-1][:-4]
+                         ) 
                 job_id = commands.getoutput(command).split()[2]
                 """Make VCF per sample here with ed_csv_to_vcf.py."""
-
-                write_file = open(outfolder + "/"
-                                  + str(item) + "_" + str(gender)
-                                  + "_" + "csv_2_vcf.sh", "w"
-                                  )
+                write_file = open("{0}/{1}_{2}_csv_2_vcf.sh".format(
+                                  outfolder,
+                                  item,
+                                  gender
+                                 ),"w")
                 write_file.write(str(qsub_call) + "\n")
-                write_file.write("#$ -hold_jid " + str(job_id) + "\n")
-                write_file.write("cd " + str(outfolder)+ "\n")
-                write_file.write("rename " + str(bam.split("/")[-1]) + " " 
-                                 + str(item)+"_" + str(settings.refset) 
-                                 + "_" + str(bam.split("/")[-1]) + " *\n"
-                                 )
-                write_file.write("python " + str(settings.csv2vcf) + " -i "
-                                 + str(outfolder) + " -t " + str(settings.template)
-                                 + " -m " + str(analysis[item]["refset"][gender])
-                                 + " --id " + str(sampleid) + "\n"
-                                 )
-                write_file.write("cp " + str(outfolder) + "/*vcf " + str(outdir) + "/" + str(item) + "\n")
+                write_file.write("#$ -hold_jid {0}\n".format(job_id))
+                write_file.write("cd {0}\n".format(outfolder))
+                write_file.write("rename {0} {1}_{2}_{3} *\n ".format(
+                                 bam.split("/")[-1],
+                                 item,
+                                 settings.refset,
+                                 bam.split("/")[-1]
+                                 ))
+                write_file.write("python {0} -i {1} -t {2} -m {3} --id {4}\n".format(
+                                 settings.csv2vcf,
+                                 outfolder,
+                                 settings.template,
+                                 analysis[item]["refset"][gender],
+                                 sampleid
+                                 ))
+                write_file.write("cp {0}/*vcf {1}/{2} \n".format(
+                                 outfolder,
+                                 outdir,
+                                 item
+                                 ))
                 write_file.close()
-                os.system("qsub " + outfolder + "/" + str(item) + "_"
-                          + str(gender) + "_" + "csv_2_vcf.sh"
-                          )
+                os.system("qsub {0}/{1}_{2}_csv_2_vcf.sh".format(outfolder,item,gender))
         """Touch done file is loop is completed"""
-        os.system("touch "+str(log_dir) + "/"+str(bam.split("/")[-1])+".done")
+        os.system("touch {0}/{1}.done".format(log_dir,bam.split("/")[-1]))
 else:
     sys.exit("Choose either make_ref or make_call")
