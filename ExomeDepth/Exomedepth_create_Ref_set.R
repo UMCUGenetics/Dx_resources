f71f3dce9d85238e1cec288172fb016b896f35b8
melferink <m.elferink@umcutrecht.nl>
2020-03-12 15:12:26 +0100

    refactor

--- ExomeDepth/Exomedepth_create_Ref_set.R
+++ ExomeDepth/Exomedepth_create_Ref_set.R
@@ -6,7 +6,7 @@ library(stringr)
 
 args = commandArgs(trailingOnly = TRUE)
 input = args[1]  #input folder
-output = args[2]  #output folder  
+output = args[2]  #output file
 name <- (substr(output, 0 ,str_length(output)-4 ))
 target.file = args[3]  #Probe target bed file  
 reference.file = args[4]  #Reference genome (fasta)   
@@ -14,11 +14,5 @@ exons = paste(args[5],sep = "")  #Exon target tsv file
 
 refbam.files <- paste0(input, dir(input, "bam$"))
 exons.hg19 = read.table(exons,sep = "\t", header = TRUE)
-my.refcounts <- getBamCounts(
-    bed.frame = exons.hg19,
-    bam.files = refbam.files,
-    include.chr = FALSE,
-    referenceFasta = reference.file
-)
+my.refcounts <- getBamCounts(bed.frame = exons.hg19, bam.files = refbam.files, include.chr = FALSE, referenceFasta = reference.file)
 save(my.refcounts,file = output)
-
