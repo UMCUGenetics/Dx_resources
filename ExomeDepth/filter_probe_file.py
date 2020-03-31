f6b1aed132965e799c3efa7d43114b8acc47ceec
melferink <m.elferink@umcutrecht.nl>
2020-03-31 16:02:04 +0200

    refactor and bug fix

--- ExomeDepth/filter_probe_file.py
+++ ExomeDepth/filter_probe_file.py
@@ -11,34 +11,35 @@ cv = int(sys.argv[4])  #Minumum coefficient of varation (CV)
 
 """ Make bam_coverage count for each BAM file first"""
 os.system("paste {0}/*bam_coverage > {0}/full_table.txt".format(wkdir))  #Make one table of all bam_coverage counts
-lines = open(str(wkdir) + "/full_table.txt","r").readlines() 
 
-for line in lines:
-    if "#" not in line:
-        splitline = line.split("\t") 
-        read_list = []
-        depth_list = []
+#lines = open(str(wkdir) + "/full_table.txt","r").readlines() 
+with open(str(wkdir) + "/full_table.txt","r") as infile:
+    for line in infile: 
+        if "#" not in line:
+            splitline = line.split("\t") 
+            read_list = []
+            depth_list = []
 
-        """Read depth stats."""
-        x=4
-        while x < len(splitline):
-            depth_list += [float(splitline[x])]
-            x += 6  #This dependants on the sambamaba output!
+            """Read depth stats."""
+            mean_coverage_column = 4
+            while  mean_coverage_column < len(splitline):
+                depth_list += [float(splitline[mean_coverage_column])]
+                mean_coverage_column += 6  #Go to next meancoverage column. note: this dependants on the sambamaba output file!
 
-        """Calculate mean, std, cv."""
-        mean_depth = mean(depth_list)
-        std_depth = stdev(depth_list)
-        if mean_depth > 0:
-            cv_depth = float(std_depth / mean_depth) * 100
+            """Calculate mean, std, cv."""
+            mean_depth = mean(depth_list)
+            std_depth = stdev(depth_list)
+            if mean_depth > 0:
+                cv_depth = (float(std_depth)/float(mean_depth)) * 100
 
-        """Print only targets that are within requirements"""
-        if mean_depth >= dp_min and cv_depth <= cv and cv_depth < cv:
-            print "{0}\t{1}\t{2}\t{3}\t{4}\t{5}".format(
-                   splitline[0].rstrip(),
-                   splitline[1].rstrip(),
-                   splitline[2].rstrip(),
-                   mean_depth,
-                   std_depth,
-                   cv_depth
-                   )
+            """Print only targets that are within requirements"""
+            if mean_depth >= dp_min and mean_depth < dp_max and cv_depth < cv:
+                print "{0}\t{1}\t{2}\t{3}\t{4}\t{5}".format(
+                       splitline[0].rstrip(),
+                       splitline[1].rstrip(),
+                       splitline[2].rstrip(),
+                       mean_depth,
+                       std_depth,
+                       cv_depth
+                      )
 
