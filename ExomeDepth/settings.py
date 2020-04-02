e95d84bf60bb06b05f76571806d550fc3a8270aa
melferink <m.elferink@umcutrecht.nl>
2020-04-02 12:53:42 +0200

    added multi statistics outputs for igv sessions

--- ExomeDepth/settings.py
+++ ExomeDepth/settings.py
@@ -1,7 +1,8 @@
 ## Settings used in run_Exomdepth.py ##
 
 # Location of repository
-cwd = "/hpc/diaggen/software/production/Dx_resources/ExomeDepth/"
+#cwd = "/hpc/diaggen/software/production/Dx_resources/ExomeDepth/"
+cwd="/hpc/diaggen/software/development/Dx_resources_ED/ExomeDepth/"
 
 # R script for RefSet and CNV calling
 call_cnv_r = str(cwd)+"Exomedepth_callCNVs.R"
@@ -9,7 +10,7 @@ create_refset_r = str(cwd)+"Exomedepth_create_Ref_set.R"
 
 #ExomeDepth CSV to VCF parser
 csv2vcf = str(cwd)+"ed_csv_to_vcf.py"
-template = str(cwd)+"template.vcf"
+vcf_template = str(cwd)+"template.vcf"
 
 # Reference genome
 reference_genome = "/hpc/diaggen/data/databases/ref_genomes/Homo_sapiens.GRCh37.GATK.illumina/Homo_sapiens.GRCh37.GATK.illumina.fasta"
@@ -19,10 +20,12 @@ r_version = "R/3.5.1"
 
 # Location of target files
 refset_dir = "/hpc/diaggen/data/databases/ExomeDepth_refset/"
-reffile_dir = "/hpc/diaggen/software/production/Dx_tracks/ExomeDepth/"
+#reffile_dir = "/hpc/diaggen/software/production/Dx_tracks/ExomeDepth/"
+reffile_dir = "/hpc/diaggen/software/development/Dx_tracks_EDlfs/ExomeDepth/"
 
 #Reference set
-refset = "Jan2020"
+#refset = "Jan2020"
+refset = "Nov2019"
 
 analysis = {"HC":
               {"refset":{"female":str(refset_dir)+"/HC_female_"+str(refset)+".EDref",
@@ -52,4 +55,9 @@ ratio_threshold_del = 0.25
 par1 = [60001,2699520]
 par2 = [154931044,155260560]
 normal_CN = {"female":{"auto":2,"chrX":2,"chrXpar":2,"chrY":0},"male":{"auto":2,"chrX":1,"chrXpar":2,"chrY":1}}
+
+# IGV session settings:
+template_xml = str(cwd)+"igv_session_template.xml"
+igv_xml = str(cwd)+"igv_xml_session.py"
+igv_settings={"ratio":[0,1,2], "log2ratio":[-2.5,0,2.5] }
 
