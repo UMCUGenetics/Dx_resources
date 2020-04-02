c06123b1eb762592ad2c6d909f0168604b4f16a0
melferink <m.elferink@umcutrecht.nl>
2020-04-02 12:53:48 +0200

    added multi statistics outputs for igv sessions

--- ExomeDepth/igv_xml_session.py
+++ ExomeDepth/igv_xml_session.py
@@ -6,19 +6,23 @@ from optparse import OptionParser
 from optparse import OptionGroup
 import settings
 
-def make_igvsession(igv_ed_umcu, igv_ed_hc, bam, vcf_hc, sample_id, vcf_SNV):
+def make_igvsession(igv_ed_umcu, igv_ed_hc, bam, vcf_hc, sample_id, vcf_SNV, axis, statistic):
     template_file = Template(open(settings.template_xml).read())
     new_session = str(sample_id) + "_igv.xml"
-    igv_ed_hc_ratio_test = str(igv_ed_hc) + "_ratio_test"
-    igv_ed_umcu_ratio_test = str(igv_ed_umcu) + "_ratio_test"
+    igv_ed_hc_test = "{0}_{1}_test".format(igv_ed_hc, statistic)
+    igv_ed_umcu_test = "{0}_{1}_test".format(igv_ed_umcu, statistic)
     bam_coverage = str(bam) + "_coverage"
     bam_junctions = str(bam) + "_junctions"
+    min_axis = axis[0]
+    mid_axis = axis[1]
+    max_axis = axis[2]
+
     substitute_dic={'session_var' : new_session, 'igv_ed_umcu' : igv_ed_umcu, 
                     'igv_ed_hc' : igv_ed_hc, 'bam':bam, 'vcf_hc' : vcf_hc, 
-                    'sample_id' : sample_id, 'igv_ed_hc_ratio_test' : igv_ed_hc_ratio_test,
-                    'igv_ed_umcu_ratio_test' : igv_ed_umcu_ratio_test, 
+                    'sample_id' : sample_id, 'igv_ed_hc_test' : igv_ed_hc_test,
+                    'igv_ed_umcu_test' : igv_ed_umcu_test, 
                     'bam_coverage' : bam_coverage, 'bam_junctions' : bam_junctions, 
-                    'vcf_SNV': vcf_SNV
+                    'vcf_SNV': vcf_SNV, 'min_axis' : min_axis, 'mid_axis' : mid_axis,'max_axis' : max_axis
                    }
     new_file = template_file.substitute(substitute_dic)
     return new_file
@@ -51,6 +55,9 @@ if __name__ == "__main__":
     bam_id = "../{0}/mapping/{1}".format(opt.sample_id,opt.bam)
     vcf_hc = "HC/HC_{0}_{1}exome_calls.vcf".format(refdate, opt.bam)
     vcf_SNV = "../fingerprint/{0}_fingerprint.vcf".format(opt.sample_id)
-    write_file = open("{0}/{1}_igv.xml".format(opt.output,opt.sample_id), "w")
-    write_file.write(make_igvsession(igv_ed_umcu, igv_ed_hc, bam_id, vcf_hc, opt.sample_id, vcf_SNV))
-    write_file.close()
+    
+    igv_settings=settings.igv_settings
+    for statistic in igv_settings:
+        write_file = open("{0}/{1}_{2}_igv.xml".format(opt.output, opt.sample_id, statistic), "w")
+        write_file.write(make_igvsession(igv_ed_umcu, igv_ed_hc, bam_id, vcf_hc, opt.sample_id, vcf_SNV, igv_settings[statistic], statistic))
+        write_file.close()
