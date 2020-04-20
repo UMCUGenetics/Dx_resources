ade8102468f296dc552b28db5a101a6cf6c7198f
melferink <m.elferink@umcutrecht.nl>
2020-04-20 08:43:19 +0200

    changed to single bam CNV calling only (removed sge)

--- ExomeDepth/run_ExomeDepth.py
+++ ExomeDepth/run_ExomeDepth.py
@@ -37,15 +37,22 @@ if __name__ == "__main__":
                      action = 'store_true', help="Make new reference set")
     group.add_option("-c", dest = "make_call",
                      action = 'store_true', help = "Call CNV from BAMs")
-    group.add_option("-i", dest = "input_bam", 
-                     metavar = "[PATH]", help = "path to input bam file")
-    group.add_option("--run", dest = "run_id",
-                     metavar = "[STRING]", help = "run_id name")
-    group.add_option("--if", dest = "input_folder",
-                     metavar = "[PATH]", help = "folder path in which contains BAM files")
+
     group.add_option("-o", dest = "output_folder",
                      default = "./", metavar = "[PATH]",
                      help = "path to output folder [default = ./]")
+    group.add_option("--bam", dest = "input_bam", 
+                     metavar = "[PATH]", help = "path to input bam file")
+    group.add_option("--run", dest = "run_id",
+                     metavar = "[STRING]", help = "run_id name")
+    group.add_option("--model", dest = "model", default = "HC",
+                     metavar = "[STRING]", help = "model HC/UMCU [default = HC]")
+    group.add_option("--sample", dest = "sample_id", 
+                     metavar = "[STRING]", help = "sampleID")
+
+
+    group.add_option("--if", dest = "input_folder",
+                     metavar = "[PATH]", help = "folder path in which contains BAM files")
     group.add_option("-g", dest = "gender_file",
                      metavar = "[PATH]",
                      help = "full path to tab delimited txt file with bam \
@@ -56,7 +63,9 @@ if __name__ == "__main__":
                      help = "reference set to be used [default = reference set in setting.py]")
     parser.add_option_group(group)
     (opt, args) = parser.parse_args()
-    
+   
+
+    """ 
     if opt.input_bam and not opt.make_ref:
         wkdir = "/".join(opt.input_bam.split("/")[0-1]).rstrip("/")
     elif opt.input_folder:
@@ -79,6 +88,29 @@ if __name__ == "__main__":
 
     if opt.run_id:
         run_id=opt.run_id  
+   
+    if opt.sample_id:
+        sample_id= opt.sample_id
+    else:
+        sys.exit("please provide sample_id")
+    """
+
+    bam = format(os.path.abspath(opt.input_bam))
+ 
+    if not opt.output_folder:
+        output_folder = format(os.path.abspath("./"))
+    else:
+        output_folder = format(os.path.abspath(opt.output_folder))
+
+    output_folder_model = "{0}/{1}".format(output_folder,opt.model)
+    if not os.path.isdir(output_folder_model):
+        os.system("mkdir "+ output_folder_model)
+
+    analysis = settings.analysis
+    ed_r = settings.call_cnv_r
+    refgenome = settings.reference_genome
+    prob = settings.probability
+    gender = settings.gender
 
     if opt.gender_file:
         gender_dic = {}
@@ -88,29 +120,20 @@ if __name__ == "__main__":
             if splitline[0] not in gender_dic:
                 gender_dic[splitline[0]] = splitline[1]
         gender_file.close()
-    
-    analysis = settings.analysis
-    ed_r = settings.call_cnv_r
-    refgenome = settings.reference_genome
-    prob = settings.probability
-    gender = settings.gender
    
     """Log all settings in setting.log file"""
-    log_dir = str(outdir)+"/logs"
-    os.system("mkdir -p "+str(log_dir))
     if opt.input_bam:
-        log_file = "{0}/{1}_settings.log".format(str(log_dir), str(opt.input_bam.split("/")[-1]))
+        log_file="{0}_{1}_{2}_settings.log".format(opt.model,opt.refset,opt.input_bam)
         write_file = open(log_file, "w") 
         (options, args) = parser.parse_args()
         for item in vars(options):
             write_file.write("{0}\t{1}\n".format(str(item), str(vars(options)[item])))
-    
         for item in dir(settings):
             if "__" not in item:
                 write_file.write("{0}\t{1}\n".format(item, str(repr(eval("settings.%s" % item)))))
         write_file.close()
-    
-    if opt.make_ref and not opt.make_call:
+  
+    if opt.make_ref and not opt.make_call:  
         """Make new reference set."""
         bams = subprocess.getoutput("find -L {0} -iname \"*.realigned.bam\"".format(wkdir)).split()
         print("Number of BAM files detected = {0}".format(len(bams)))
@@ -130,7 +153,7 @@ if __name__ == "__main__":
         """Make folder per gender + analysis, and soflink BAMs in these folders."""
         for target in analysis:
             for item in ref_gender_dic:
-                folder = "{0}/{1}_{2}_{3}".format(outdir, target, item, str(wkdir).split("/")[-1])  
+                folder = "{0}/{1}_{2}_{3}".format(output_folder, target, item, str(wkdir).split("/")[-1])  
                 output_id = "{0}_{1}_{2}.EDref".format(target, item, prefix)
                 os.system("mkdir -p {0}".format(folder))
                 for bam in ref_gender_dic[item]:
@@ -144,12 +167,9 @@ if __name__ == "__main__":
                                  settings.reference_genome,
                                  analysis[target]["exon_bed"]
                                  ))
-    elif opt.make_call and not opt.make_ref:  # Call CNV from BAMs
-        """Make CNV call on sample(s)."""
 
-        bam = format(os.path.abspath(opt.input_bam))
-
-        """Get gender from chrY read count ratio."""
+    elif opt.make_call and not opt.make_ref:  
+        """Call CNV from BAMs"""
         gender = get_gender(bam)
         if opt.gender_file:  # overrule gender as given in gender_file
            for item in gender_dic:
@@ -164,59 +184,53 @@ if __name__ == "__main__":
                 gender = "female"
             else:
                 sys.exit("Sample {0} has a unknown gender and will not be analysed".format(bam.split("/")[-1]))
-                
-        for item in analysis:
-            sampleid = bam.split("/")[-1].split("_")[0]
-            outfolder = "{0}/{1}/{1}_{2}".format(outdir, item, bam.split("/")[-1])
-            os.system("mkdir -p {0}".format(outfolder))
-            os.chdir(outfolder)
 
-            os.system("module load {0} && Rscript {1} {2} {3} {4} {5} {6} {7}".format(
-                       settings.r_version,
-                       ed_r,
-                       analysis[item]["refset"][gender],
-                       analysis[item]["target_bed"],
-                       refgenome,
-                       analysis[item]["exon_bed"],
-                       str(prob[str(item)]),
-                       bam
-                       ))
-            os.system("rename {0} {1}_{2}_{3} *".format(
-                       bam.split("/")[-1],
-                       item,
+        """Perform ExomeDepth analysis"""
+        action = ("module load {0} && Rscript {1} {2} {3} {4} {5} {6} {7} {8} {9}".format(
+                   settings.r_version,
+                   ed_r,
+                   analysis[opt.model]["refset"][gender],
+                   analysis[opt.model]["target_bed"],
+                   refgenome,
+                   analysis[opt.model]["exon_bed"],
+                   prob[str(opt.model)],
+                   bam,
+                   opt.model,
+                   opt.refset
+                  ))
+
+        print(action)
+        os.system(action)
+
+        """Move output to specifiek folder"""
+        os.system("mv {0}/* {1}".format(output_folder,output_folder_model))
+        
+        """Perform csv to vcf conversion """
+        csv_file="{0}/{1}_{2}_{3}_exome_calls.csv".format(output_folder_model,opt.model,opt.refset,opt.input_bam)
+        action = ("python {0} --csv {1} --refset {2} --model {3} --gender {4} --sampleid {5} --template {6}".format(
+                   settings.csv2vcf,
+                   csv_file,
+                   opt.refset,
+                   opt.model,
+                   gender,
+                   opt.sample_id,
+                   settings.vcf_template
+                  ))
+        print (action)
+        os.system(action)
+
+        """Make IGV session xml """ 
+        if opt.model == "HC": ## Need to re-write to Ninja for proper implementation? Template + add model 'block' to xml 
+            action = ("python {0} --bam {1} -o {2} --sampleid {3} --template {4} --refdate {5} --runid {6}".format(
+                       settings.igv_xml,
+                       opt.input_bam,
+                       output_folder,
+                       opt.sample_id,
+                       settings.igv_xml,
                        opt.refset,
-                       bam.split("/")[-1]
-                       ))
-
-            os.system("python {0} -i {1} -t {2} -m {3} --id={4}".format(
-                       settings.csv2vcf,
-                       outfolder,
-                       settings.vcf_template,
-                       analysis[item]["refset"][gender],
-                       sampleid
-                       ))
-
-            os.system("cp {0}/*vcf {1}/{2}".format(
-                       outfolder,
-                       outdir,
-                       item
-                       ))
-
-        if opt.input_bam:  #Make IGV session. Note this is only possible for single sample processing.
-            sampleid = bam.split("/")[-1].split("_")[0]
-            bam_file = bam.split("/")[-1]
-            os.system("python {0} -b {1} -o {2} -i {3} -t {4} -m {5} -r {6}".format(
-                       settings.igv_xml,
-                       bam_file,
-                       outdir,
-                       sampleid,
-                       settings.igv_xml,
-                       analysis[item]["refset"][gender],
-                       run_id
-                       )
-                      )                
-
-        """Touch done file is loop is completed"""
-        os.system("touch {0}/{1}.done".format(log_dir, bam.split("/")[-1]))
+                       opt.run_id
+                      ))
+            print(action)
+            os.system(action)
     else:
         sys.exit("Choose either make_ref or make_call")
