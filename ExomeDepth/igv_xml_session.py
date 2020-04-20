632f9bc282888240b7aff87cd15ff8fb34e070a9
melferink <m.elferink@umcutrecht.nl>
2020-04-20 08:40:28 +0200

    refactor

--- ExomeDepth/igv_xml_session.py
+++ ExomeDepth/igv_xml_session.py
@@ -26,22 +26,22 @@ def make_igvsession(igv_ed_umcu, igv_ed_hc, bam, vcf_hc, sample_id, vcf_SNV, axi
 if __name__ == "__main__":
     parser = OptionParser()
     group = OptionGroup(parser, "Main options")
-    group.add_option("-b", dest = "bam", metavar = "[PATH]",
+    group.add_option("--bam", dest = "bam", metavar = "[PATH]",
                      help = "bam file name"
                      )
     group.add_option("-o", dest = "output", metavar = "[PATH]",
                      help = "output_folder"
                      )
-    group.add_option("-i", dest = "sample_id", metavar = "[STRING]",
+    group.add_option("--sampleid", dest = "sample_id", metavar = "[STRING]",
                      help = "sampleid name"
                      )
-    group.add_option("-t", dest = "template", metavar = "[STRING]",
+    group.add_option("--template", dest = "template", metavar = "[STRING]",
                      help = "Path to template XML"
                      )
-    group.add_option("-m", dest = "callmodel", metavar = "[STRING]",
-                     help = "Name of calling model used (order: [Model]_[Gender]_[Date].EDRef)"
+    group.add_option("--refdate", dest = "ref_date", metavar = "[STRING]",
+                     help = "Date of the used reference set)"
                      )
-    group.add_option("-r", dest = "run_id", metavar = "[STRING]",
+    group.add_option("--runid", dest = "run_id", metavar = "[STRING]",
                      help = "Name of run_id"
                      )
     parser.add_option_group(group)
@@ -49,13 +49,11 @@ if __name__ == "__main__":
 
     output_folder = opt.output
     run_id = opt.run_id
-    refdate = opt.callmodel.split("/")[-1].split("_")[2].split(".")[0]
-    igv_ed_umcu = "UMCU/UMCU_{0}/UMCU_{1}_{0}_ref.igv".format(opt.bam, refdate)
-    igv_ed_hc = "HC/HC_{0}/HC_{1}_{0}_ref.igv".format(opt.bam, refdate)
-    #bam_id = "../{0}/mapping/{1}".format(opt.sample_id,opt.bam)
+
+    igv_ed_umcu = "UMCU/UMCU_{0}/UMCU_{1}_{0}_ref.igv".format(opt.bam, opt.ref_date)
+    igv_ed_hc = "HC/HC_{0}/HC_{1}_{0}_ref.igv".format(opt.bam, opt.ref_date)
     bam_id = "../bam_files/{0}".format(opt.bam)
-    vcf_hc = "HC/HC_{0}_{1}exome_calls.vcf".format(refdate, opt.bam)
-    #vcf_SNV = "../single_sample_vcf/{0}.filtered_variants.vcf".format(opt.sample_id)
+    vcf_hc = "HC/HC_{0}_{1}exome_calls.vcf".format(opt.ref_date, opt.bam)
     vcf_SNV = "../single_sample_vcf/{0}_{1}.vcf".format(opt.sample_id,run_id)
     igv_settings = settings.igv_settings
     for statistic in igv_settings:
