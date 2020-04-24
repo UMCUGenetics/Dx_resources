5ead698ba75992a30b31f6c3dc9067bf90361e8a
melferink <m.elferink@umcutrecht.nl>
2020-04-24 11:44:41 +0200

    refactor

--- ExomeDepth/igv_xml_session.py
+++ ExomeDepth/igv_xml_session.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
+import argparse
 from string import Template
 import settings
-import argparse
 
 def make_igvsession(igv_ed_umcu, igv_ed_hc, bam, vcf_hc, sample_id, vcf_SNV, axis, statistic):
     template_file = Template(open(settings.template_xml).read())
@@ -16,7 +16,7 @@ def make_igvsession(igv_ed_umcu, igv_ed_hc, bam, vcf_hc, sample_id, vcf_SNV, axi
     substitute_dic={'session_var' : new_session, 'igv_ed_umcu' : igv_ed_umcu, 'igv_ed_hc' : igv_ed_hc, 
                     'bam':bam, 'vcf_hc' : vcf_hc, 'sample_id' : sample_id, 'igv_ed_hc_test' : igv_ed_hc_test,
                     'igv_ed_umcu_test' : igv_ed_umcu_test, 'bam_coverage' : bam_coverage, 'bam_junctions' : bam_junctions, 
-                    'vcf_SNV' : vcf_SNV, 'min_axis' : min_axis, 'mid_axis' : mid_axis,'max_axis' : max_axis,
+                    'vcf_SNV' : vcf_SNV, 'min_axis' : min_axis, 'mid_axis' : mid_axis, 'max_axis' : max_axis,
                     'ratioid_UMCU' : ratioid_UMCU, 'ratioid_HC': ratioid_HC 
                    }
     new_file = template_file.substitute(substitute_dic)
@@ -33,18 +33,17 @@ if __name__ == "__main__":
     parser.add_argument('runid', help='Run ID')
     parser.add_argument('--batch', action='store_true', help='option for batch processing')
     args = parser.parse_args()
-    output_folder = args.output
 
     igv_settings = settings.igv_settings
     igv_ed_umcu = "igv_tracks/UMCU_{0}_{1}_ref.igv".format(args.refdate, args.bam)
     igv_ed_hc = "igv_tracks/HC_{0}_{1}_ref.igv".format(args.refdate, args.bam)
     vcf_hc = "HC/HC_{0}_{1}_exome_calls.vcf".format(args.refdate, args.bam)
     if args.batch: #For re-analysis based on IAP
-        bam_id = "../{0}/mapping/{1}".format(args.sampleid,args.bam)
+        bam_id = "../{0}/mapping/{1}".format(args.sampleid, args.bam)
         vcf_SNV = "../single_sample_vcf/{0}.filtered_variants.vcf".format(args.sampleid)
     else: #For NF pipeline.
         bam_id = "../bam_files/{0}.bam".format(args.sampleid)
-        vcf_SNV = "../single_sample_vcf/{0}_{1}.vcf".format(args.sampleid,args.runid)
+        vcf_SNV = "../single_sample_vcf/{0}_{1}.vcf".format(args.sampleid, args.runid)
  
     for statistic in igv_settings:
         write_file = open("{0}/{1}_{2}_igv.xml".format(args.output, args.sampleid, statistic), "w")
