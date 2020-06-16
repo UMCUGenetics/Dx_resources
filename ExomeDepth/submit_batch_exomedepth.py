ac3ae5646adef6ad7e048ecf5f00b98472b5335d
melferink <m.elferink@umcutrecht.nl>
2020-06-16 13:40:30 +0200

    included iap/nf options

--- ExomeDepth/submit_batch_exomedepth.py
+++ ExomeDepth/submit_batch_exomedepth.py
@@ -12,15 +12,28 @@ def process(bam):
     os.system("ln -sd {bam} {output}/{sample}/{bamfile}".format(bam = bam, bamfile = bamfile, sample = sampleid, output = args.outputfolder))
     os.system("ln -sd {bam}.bai {output}/{sample}/{bamfile}.bai".format(bam = bam, bamfile = bamfile, sample = sampleid, output = args.outputfolder))
     os.chdir("{output}/{sample}".format(output = args.outputfolder, sample = sampleid))
-    action = "python {exomedepth} callcnv {output}/{sample} {inputbam} {run} {sample} {refset} --expectedCNVlength {length} --batch".format(
-        exomedepth = args.exomedepth,
-        output = args.outputfolder,
-        inputbam = bamfile,
-        run = args.inputfolder.rstrip("/").split("/")[-1],
-        sample = bam.split("/")[-1].split("_")[0],
-        refset = args.refset,
-        length = args.expectedCNVlength
+
+    if args.pipeline == "iap":
+        action = "python {exomedepth} callcnv {output}/{sample} {inputbam} {run} {sample} {refset} --expectedCNVlength {length} --pipeline iap".format(
+            exomedepth = args.exomedepth,
+            output = args.outputfolder,
+            inputbam = bamfile,
+            run = args.inputfolder.rstrip("/").split("/")[-1],
+            sample = bam.split("/")[-1].split("_")[0],
+            refset = args.refset,
+            length = args.expectedCNVlength
         )
+    elif args.pipeline == "nf":
+        action = "python {exomedepth} callcnv {output}/{sample} {inputbam} {run} {sample} {refset} --expectedCNVlength {length} --pipeline nf".format(
+            exomedepth = args.exomedepth,
+            output = args.outputfolder,
+            inputbam = bamfile,
+            run = args.inputfolder.rstrip("/").split("/")[-1],
+            sample = bam.split("/")[-1].split("_")[0],
+            refset = args.refset,
+            length = args.expectedCNVlength
+        )
+
     os.system(action)
 
     os.chdir("{output}".format(output = args.outputfolder))
@@ -42,16 +55,16 @@ if __name__ == "__main__":
     parser.add_argument('inputfolder', help='Path to input folder containing BAM files to process')
     parser.add_argument('outputfolder', help='Path to output folder')
     parser.add_argument('simjobs', help='number of simultanious samples to proces. Note: make sure similar threads are reseved in session!')
-    parser.add_argument('--bamtype', default='realigned', choices=['bam', 'realigned'], help='type of BAM to be searched bam = all bam (default), realigned = realigned.bam specific')
+    parser.add_argument('--pipeline', default='iap', choices=['nf', 'iap'], help='pipeline used for sample processing (nf = nexflow, IAP = illumina analysis pipeline')
     parser.add_argument('--refset', default = settings.refset, help='Reference set to be used')
     parser.add_argument('--exomedepth', default = "/hpc/diaggen/software/production/Dx_resources/ExomeDepth/run_ExomeDepth.py", help='Full path to exomedepth script')
     parser.add_argument('--expectedCNVlength',default=settings.expectedCNVlength, help='expected CNV length (basepairs) taken into account by ExomeDepth [default expectedCNVlength in settings.py]')
     args = parser.parse_args()
 
 
-    if args.bamtype == "realigned":
+    if args.pipeline == "iap":
         bams = subprocess.getoutput("find -L {0} -type f -name \"*.realigned.bam\" -not -ipath \"*exomedepth*\"".format(args.inputfolder)).split()
-    elif args.bamtype == "bam":
+    elif args.pipeline == "nf":
         bams = subprocess.getoutput("find -L {0} -type f -name \"*.bam\" -not -ipath \"*exomedepth*\"".format(args.inputfolder)).split()
 
     print("Number of BAM files = "+str(len(bams)))
