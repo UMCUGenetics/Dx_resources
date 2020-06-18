6d8e61dabee74b58672dc95dac29321d15f6fd0e
melferink <m.elferink@umcutrecht.nl>
2020-06-18 08:10:36 +0200

    bugfix and python3

--- ExomeDepth/slice_bed_file.py
+++ ExomeDepth/slice_bed_file.py
@@ -2,21 +2,22 @@
 import sys
 
 bed_file = open(sys.argv[1],"r")  # BED file
-tlen = int(sys.argv[2]) # targeted length of target/exons
+tlen = float(sys.argv[2]) # targeted length of target/exons
 
 for line in bed_file:
     splitline = line.split()
-    leng = int(splitline[2]) - int(splitline[1])
-    if leng >= (2*tlen):
-        regions = int(round(float(leng)/float(tlen)))
-        chunk = int(round(leng/regions))
+    leng = float(splitline[2]) - float(splitline[1])
+    if leng >= (2 * tlen):
+        regions = round(leng / tlen)
+        chunk = round(leng / regions)
         start = int(splitline[1])
         for i in range(0, regions - 1):
-           stop = start + chunk
-           print splitline[0] + "\t" + str(start) + "\t" + str(stop)
-           start += chunk + 1
-        print splitline[0] + "\t" + str(start) + "\t" + str(splitline[2])
+            stop = start + chunk
+            print("{chr}\t{start}\t{stop}".format(chr=splitline[0], start=start, stop=stop))
+            start += chunk 
+        print("{chr}\t{start}\t{stop}".format(chr=splitline[0], start=start, stop=splitline[2]))
     else:
-        print line.rstrip()
-       
+        print(line.rstrip())
+
+bed_file.close()
 
