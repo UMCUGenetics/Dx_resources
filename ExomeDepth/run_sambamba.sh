701c10eb34ff22a01b7810e66b1ca4123484d94a
melferink <m.elferink@umcutrecht.nl>
2020-11-23 11:41:56 +0100

    refactor

--- ExomeDepth/run_sambamba.sh
+++ ExomeDepth/run_sambamba.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-folder=$1
-bed=$2
-mail=$3
+sambamba=$1
+folder=$2
+bed=$3
+mail=$4
 
-for bam in $folder/*realigned.bam ; do
-    #echo "/hpc/local/CentOS7/cog_bioinf/sambamba_v0.6.5/sambamba_v0.6.5 depth region $bam -L $bed -m -q 10 -F \"mapping_quality >= 20 and not duplicate and not failed_quality_control and not secondary_alignment\" -t 6 -o $bam\_coverage"|qsub -cwd -pe threaded 6 -l h_vmem=40G -l h_rt=10:00:00 -m a -M m.elferink@umcutrecht.nl 
-
-    sbatch -t 4:0:00 --gres=tmpspace:20G --mem=20G -c 2  --mail-type=FAIL --mail-user=$mail --wrap="/hpc/local/CentOS7/cog_bioinf/sambamba_v0.6.5/sambamba_v0.6.5 depth region $bam -L $bed -m -q 10 -F \"mapping_quality >= 20 and not duplicate and not failed_quality_control and not secondary_alignment\" -t 6 -o $bam\_coverage"
+for bam in $folder/*.bam ; do
+    sbatch -t 4:0:00 --gres=tmpspace:20G --mem=20G -c 2  --mail-type=FAIL --mail-user=$mail --wrap="$sambamba depth region $bam -L $bed -m -q 10 -F \"mapping_quality >= 20 and not duplicate and not failed_quality_control and not secondary_alignment\" -t 6 -o $bam\_coverage"
 
 done
