739d666cb5dde4e4a7ab288944a847ca1b73d0ee
melferink <m.elferink@umcutrecht.nl>
2020-11-24 14:07:14 +0100

    refactor

--- ExomeDepth/check_gender_bam.py
+++ ExomeDepth/check_gender_bam.py
@@ -11,29 +11,33 @@ def valid_read(read):
     else:
         return False
 
-def get_gender(bam):
+def get_gender(bam, arguments):
     """Determine chrY ratio based on read count in bam (excl PAR)."""
     workfile = pysam.AlignmentFile(bam, "rb")
-    locusY = settings.locusY
-    locusX = settings.locusX
-    yreads = float(sum([valid_read(read) for read in workfile.fetch(region=locusY)]))
-    xreads = float(sum([valid_read(read) for read in workfile.fetch(region=locusX)]))
-    total = float(workfile.mapped)
-    yratio = float("%.2f" % ((yreads / total) * 100))
-    xratio = float("%.2f" % ((xreads / total) * 100))
-
-    if yratio <= float(settings.yratio[0]) and xratio >= float(settings.xratio[1]):
-        return "female", yratio, xratio
-    elif yratio >= float(settings.yratio[1]) and xratio <= float(settings.xratio[0]):
-        return "male", yratio, xratio
+    y_reads = float(sum([valid_read(read) for read in workfile.fetch(region=arguments.locusy)]))
+    x_reads = float(sum([valid_read(read) for read in workfile.fetch(region=arguments.locusx)]))
+    total_reads = float(workfile.mapped)
+    y_ratio = float("%.2f" % ((y_reads / total_reads) * 100))
+    x_ratio = float("%.2f" % ((x_reads / total_reads) * 100))
+    
+    if y_ratio <= arguments.y_ratio_female and x_ratio >= arguments.x_ratio_female:
+        return "female\t{y_ratio}\t{x_ratio}".format(y_ratio=y_ratio, x_ratio=x_ratio)
+    elif y_ratio >= arguments.y_ratio_male and x_ratio <= arguments.x_ratio_male:
+        return "male\t{y_ratio}\t{x_ratio}".format(y_ratio=y_ratio, x_ratio=x_ratio)
     else:
-        return "unknown or other sex chromosome combination", yratio, xratio
+        return "unknown or other sex chromosome combination\t{y_ratio}\t{x_ratio}".format(y_ratio=y_ratio, x_ratio=x_ratio)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('inputfolder', help='Path to root folder of analysis')
-    args = parser.parse_args()
+    parser.add_argument('--locusy', default=settings.locusy, help='Coordinates for includes region on chromosome X (default = locusy in settings.py)')
+    parser.add_argument('--locusx', default=settings.locusx, help='Threshold for maximum allowed CNV calls (default = locusx in settings.py)')
+    parser.add_argument('--y_ratio_female', default=settings.y_ratio[0], type=float, help='Maximum Y ratio threshold females (default = y_ratio[0] in settings.py)')
+    parser.add_argument('--y_ratio_male', default=settings.y_ratio[1], type=float, help='Minimum Y ratio threshold males (default = y_ratio[1] in settings.py)')
+    parser.add_argument('--x_ratio_male', default=settings.x_ratio[0], type=float, help='Maximum X ratio threshold males (default = x_ratio[0] in settings.py)')
+    parser.add_argument('--x_ratio_female', default=settings.x_ratio[1], type=float, help='Minimum X ratio threshold females (default = x_ratio[1] in settings.py)')
+    arguments = parser.parse_args()
 
-    bams = glob.glob("{}/**/*.bam".format(args.inputfolder), recursive=True)
+    bams = glob.glob("{}/**/*.bam".format(arguments.inputfolder), recursive=True)
     for bam in bams:
-        print(bam, *get_gender(bam))
+        print("{bam}\t{gender_result}".format(bam=bam, gender_result=get_gender(bam, arguments)))
