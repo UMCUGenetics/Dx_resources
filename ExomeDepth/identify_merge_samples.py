e5d0e3424f96b8e933f4c12b2ec00c7cb95c9454
melferink <m.elferink@umcutrecht.nl>
2020-11-27 11:13:42 +0100

    PR changes

--- ExomeDepth/identify_merge_samples.py
+++ ExomeDepth/identify_merge_samples.py
@@ -10,31 +10,30 @@ def detect_merge(arguments):
     for bam in bams:
         skip_file_keywords = ['tmp', 'exomedepth', 'work']
         if not any(keyword in bam.lower() for keyword in skip_file_keywords): # Skip files in folder with keywords
-            bam_file = pysam.AlignmentFile(bam, "rb")
+            with pysam.AlignmentFile(bam, "rb") as bam_file:
+                """ Extract sample_ID """
+                sample_id = re.split('_|\.', bam.split("/")[-1])[0]
 
-            """ Extract sample_ID """
-            sample_id = re.split('_|\.', bam.split("/")[-1])[0]
+                """ Extract run ID """
+                run_id = bam.split("/")[-3]
+                if run_id == sample_id:  # Run is old IAP run
+                    run_id = bam.split("/")[-4] 
 
-            """ Extract run ID """
-            run_id = bam.split("/")[-3]
-            if run_id == sample_id:  # Run is old IAP run
-                run_id = bam.split("/")[-4] 
+                """ Removes deck (A/B) from run_barcode ID. This information is not present in BAM file """
+                run_barcode = re.sub('^[A|B]', '', run_id.split("_")[3])
 
-            """ Removes deck (A/B) from run_barcode ID. This information is not present in BAM file """
-            run_barcode = re.sub('^[A|B]', '', run_id.split("_")[3])
+                """ Compare barcode ID in BAM with run barcode ID(s) of sample """
+                sample_barcodes = list(set([read_group['PU'] for read_group in bam_file.header['RG']]))
+                sample_barcode = re.sub('^[A|B]', '', sample_barcodes[0])
 
-            """ Compare barcode ID in BAM with run barcode ID(s) of sample """
-            sample_barcodes = list(set([read_group['PU'] for read_group in bam_file.header['RG']]))
-            sample_barcode = re.sub('^[A|B]', '', sample_barcodes[0])
-
-            if len(sample_barcodes) > 1:  # Sample consisting of multiple sequence runs is considered merge sample
-                merge_file.write("{sample_id}\t{run_id}\t{reason}\n".format(
-                    sample_id=sample_id, run_id=run_id, reason="multiple_runs"
-                    ))
-            elif run_barcode != sample_barcode:
-                merge_file.write("{sample_id}\t{run_id}\t{reason}\t{sample_barcode}\n".format(
-                    sample_id=sample_id, run_id=run_id, reason="different_barcode", sample_barcode=sample_barcode
-                    ))
+                if len(sample_barcodes) > 1:  # Sample consisting of multiple sequence runs is considered merge sample
+                    merge_file.write("{sample_id}\t{run_id}\t{reason}\n".format(
+                        sample_id=sample_id, run_id=run_id, reason="multiple_runs"
+                        ))
+                elif run_barcode != sample_barcode:
+                    merge_file.write("{sample_id}\t{run_id}\t{reason}\t{sample_barcode}\n".format(
+                        sample_id=sample_id, run_id=run_id, reason="different_barcode", sample_barcode=sample_barcode
+                        ))
 
     merge_file.close()
 
