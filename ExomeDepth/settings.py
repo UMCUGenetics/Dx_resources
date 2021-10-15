c698b91eb10499c0b4bddec904cf7a476355590b
melferink <m.elferink@umcutrecht.nl>
2021-10-15 17:12:50 +0200

    split in single and family template xml

--- ExomeDepth/settings.py
+++ ExomeDepth/settings.py
@@ -60,7 +60,9 @@ expectedCNVlength = 1000000
 chromosome_order = {'1':0, '2':1, '3':2, '4':3, '5':4, '6':5, '7':6, '8':7, '9':8, '10':9, '11':10, '12':11, '13':12, '14':13, '15':14, '16':15, '17':16, '18':17, '19':18, '20':19, '21':20, '22':21, 'X':22, 'Y':23, 'MT':24}
 
 # IGV session settings:
-template_xml = str(cwd)+"igv_session_template.xml"
+template_single_xml = str(cwd)+"igv_session_template_single.xml"
+template_family_xml = str(cwd)+"igv_session_template_family.xml"
+
 igv_xml = str(cwd)+"igv_xml_session.py"
 igv_settings = {"ratio":[0, 1, 2], "log2ratio":[-2.5, 0, 2.5] }
 
