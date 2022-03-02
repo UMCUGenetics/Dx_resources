8febd4ffb37bf0d83a31cdd9efb32eb7ecb62267
melferink <m.elferink@umcutrecht.nl>
2022-03-02 14:23:13 +0100

    flake8 layout refactor

--- ExomeDepth/exomedepth_summary.py
+++ ExomeDepth/exomedepth_summary.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Create ExomeDepth Metric summary file')
     parser.add_argument('exomedepth_logs', type=argparse.FileType('r'), nargs='*', help='Exomedepth log files')
     arguments = parser.parse_args()
-    stats_dic = {"CR":[], "PD":[], "TC":[]}
+    stats_dic = {"CR": [], "PD": [], "TC": []}
     for exomedepth_qc_file in arguments.exomedepth_logs:
-        for line in exomedepth_qc_file: 
+        for line in exomedepth_qc_file:
             splitline = line.split()
             correlation = float(splitline[3])
             deldupratio = float(splitline[4])
@@ -26,7 +26,7 @@ if __name__ == "__main__":
                  totalcount="%.0f" % totalcount,
                  warnings="\t".join(warnings)
                  )),
-     
+
     print("\r")
     print("#Average_CR={}\r".format("%.4f" % statistics.mean(stats_dic["CR"]))),
     print("#Average_PD={}\r".format("%.2f" % statistics.mean(stats_dic["PD"]))),
