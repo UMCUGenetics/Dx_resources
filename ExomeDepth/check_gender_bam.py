7d200649f8fe19d1d9bb7d4e8521a9892a4006a9
melferink <m.elferink@umcutrecht.nl>
2022-03-02 14:09:15 +0100

    flake8 layout refactor

--- ExomeDepth/check_gender_bam.py
+++ ExomeDepth/check_gender_bam.py
@@ -4,11 +4,13 @@ import glob
 import pysam
 import settings
 
+
 def is_valid_read(read):
     """Check if a read is properly mapped."""
     if (read.mapping_quality >= 20 and read.reference_end and read.reference_start):
         return True
     return False
+
 
 def get_gender(bam, arguments):
     """Determine chrY ratio based on read count in bam (excl PAR)."""
@@ -18,8 +20,9 @@ def get_gender(bam, arguments):
         total_reads = float(bam_file.mapped)
         y_ratio = (y_reads / total_reads) * 100
         x_ratio = (x_reads / total_reads) * 100
-    
-        if y_ratio <= arguments.gender_determination_y_ratio_female and x_ratio >= arguments.gender_determination_x_ratio_female:
+
+        if (y_ratio <= arguments.gender_determination_y_ratio_female
+           and x_ratio >= arguments.gender_determination_x_ratio_female):
             return "female\t{y_ratio:.2f}\t{x_ratio:.2f}".format(y_ratio=y_ratio, x_ratio=x_ratio)
 
         elif y_ratio >= arguments.gender_determination_y_ratio_male and x_ratio <= arguments.gender_determination_x_ratio_male:
@@ -28,15 +31,44 @@ def get_gender(bam, arguments):
         else:
             return "unknown or other sex chromosome combination\t{y_ratio}\t{x_ratio}".format(y_ratio=y_ratio, x_ratio=x_ratio)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('inputfolder', help='Path to root folder of analysis')
-    parser.add_argument('--gender_determination_locus_y', default=settings.gender_determination_locus_y, help='Coordinates for includes region on chromosome X (default = gender_determination_locus_y in settings.py)')
-    parser.add_argument('--gender_determination_locus_x', default=settings.gender_determination_locus_x, help='Threshold for maximum allowed CNV calls (default = gender_determination_locus_x in settings.py)')
-    parser.add_argument('--gender_determination_y_ratio_female', default=settings.gender_determination_y_ratio[0], type=float, help='Maximum Y ratio threshold females (default = gender_determination_y_ratio[0] in settings.py)')
-    parser.add_argument('--gender_determination_y_ratio_male', default=settings.gender_determination_y_ratio[1], type=float, help='Minimum Y ratio threshold males (default = gender_determination_y_ratio[1] in settings.py)')
-    parser.add_argument('--gender_determination_x_ratio_male', default=settings.gender_determination_x_ratio[0], type=float, help='Maximum X ratio threshold males (default = gender_determination_x_ratio[0] in settings.py)')
-    parser.add_argument('--gender_determination_x_ratio_female', default=settings.gender_determination_x_ratio[1], type=float, help='Minimum X ratio threshold females (default = gender_determination_x_ratio[1] in settings.py)')
+    parser.add_argument(
+        '--gender_determination_locus_y',
+        default=settings.gender_determination_locus_y,
+        help='Coordinates for includes region on chromosome X (default = gender_determination_locus_y in settings.py)'
+    )
+    parser.add_argument(
+        '--gender_determination_locus_x',
+        default=settings.gender_determination_locus_x,
+        help='Threshold for maximum allowed CNV calls (default = gender_determination_locus_x in settings.py)'
+    )
+    parser.add_argument(
+        '--gender_determination_y_ratio_female',
+        default=settings.gender_determination_y_ratio[0],
+        type=float,
+        help='Maximum Y ratio threshold females (default = gender_determination_y_ratio[0] in settings.py)'
+    )
+    parser.add_argument(
+        '--gender_determination_y_ratio_male',
+        default=settings.gender_determination_y_ratio[1],
+        type=float,
+        help='Minimum Y ratio threshold males (default = gender_determination_y_ratio[1] in settings.py)'
+    )
+    parser.add_argument(
+        '--gender_determination_x_ratio_male',
+        default=settings.gender_determination_x_ratio[0],
+        type=float,
+        help='Maximum X ratio threshold males (default = gender_determination_x_ratio[0] in settings.py)'
+    )
+    parser.add_argument(
+        '--gender_determination_x_ratio_female',
+        default=settings.gender_determination_x_ratio[1],
+        type=float,
+        help='Minimum X ratio threshold females (default = gender_determination_x_ratio[1] in settings.py)'
+    )
     arguments = parser.parse_args()
 
     bams = glob.glob("{}/**/*.bam".format(arguments.inputfolder), recursive=True)
