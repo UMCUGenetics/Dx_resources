012310076dac53313a70a617e14fd55e67c917b0
melferink <m.elferink@umcutrecht.nl>
2022-04-26 09:51:36 +0200

    moved functions to database/functions.py

--- ExomeDepth/exomedepth_db.py
+++ ExomeDepth/exomedepth_db.py
@@ -3,129 +3,36 @@
 import argparse
 from database.database import connect_database
 from database.models import Sample
+import database.functions
 
-import pysam
 import settings
 
 
 def add_sample_to_db(args):
-    Session = connect_database()
-    flowcell_id = get_flowcell_id(args.flowcell_id)
-    with Session() as session:
-        if not session.query(Sample).filter(Sample.sample == args.sample_id).filter(Sample.flowcell == flowcell_id).all():
-            sample = Sample(sample=args.sample_id, flowcell=flowcell_id, refset=args.refset)
-            session.add(sample)
-            session.commit()
-            print("## Sample {0} added to database with flowcell {1} and with refset {2}".format(
-                args.sample_id, flowcell_id, args.refset)
-            )
+    database.functions.add_sample_to_db(args.flowcell_id, args.sample_id, args.refset)
 
 
 def change_refset_in_db(args):
-    Session = connect_database()
-    with Session() as session:
-        sample_update = (
-            session.query(Sample)
-            .filter(Sample.sample == args.sample_id)
-            .filter(Sample.flowcell == args.flowcell_id)
-            .one()
-        )
-        sample_update.refset = args.refset
-        session.add(sample_update)
-        session.commit()
-        print("## Changed refset of sample {0} with flowcell {1} to refset {2}".format(
-            args.sample_id, args.flowcell_id, args.refset)
-        )
-
-
-def print_all_samples(args):
-    Session = connect_database()
-    with Session() as session:
-        print("Name\tFlowcell\tRefset\tFamilyID")
-        for item in session.query(Sample):
-            print("{0}\t{1}\t{2}".format(item.sample, item.flowcell, item.refset))
-
-
-def print_refset(flowcell_id, sample_id):
-    Session = connect_database()
-    with Session() as session:
-        print(
-            session.query(Sample)
-            .filter(Sample.sample == sample_id)
-            .filter(Sample.flowcell == flowcell_id)
-            .one()
-            .refset
-        )
+    database.functions.change_refset_in_db(args.flowcell_id, args.sample_id, args.refset)
 
 
 def query_refset(args):
-    flowcell_id = get_flowcell_id(args.flowcell_id)
-    print_refset(flowcell_id, args.sample_id)
+    database.functions.query_refset(args.flowcell_id, args.sample_id)
 
 
 def query_refset_bam(args):
-    flowcell_id = get_flowcelid_bam(args.bam)
-    sample_id = get_sample_id(args.bam)
-    print_refset(flowcell_id, sample_id)
-
-
-def get_flowcelid_bam(bam):
-    with pysam.AlignmentFile(bam, "rb") as workfile:
-        readgroups = []
-        for readgroup in workfile.header['RG']:
-            if readgroup['PU'] not in readgroup:
-                readgroups.append(readgroup['PU'])
-        readgroups = list(set(readgroups))
-        flowcell_id = "_".join(readgroups)
-    return flowcell_id
+    database.functions.query_refset_bam(args.bam)
 
 
 def delete_sample_db(args):
-    Session = connect_database()
-    flowcell_id = get_flowcell_id(args.flowcell_id)
-    with Session() as session:
-        samples = session.query(Sample).filter(Sample.sample == args.sample_id).filter(Sample.flowcell == flowcell_id)
-        if samples:
-            samples.delete()
-            session.commit()
-            print("Deleted sample {0} with flowcell {1}.".format(args.sample_id, flowcell_id))
-        else:
-            print("Sample {0} with flowcell {1} not in database.".format(args.sample_id, flowcell_id))
-
-
-def get_flowcell_id(flowcellsarg):
-    flowcells = list(set(flowcellsarg))
-    flowcells.sort()
-    flowcell_id = "_".join(flowcells)
-    return flowcell_id
+    database.functions.delete_sample_db(args.flowcell_id, args.sample_id)
 
 
 def add_sample_to_db_and_return_refset_bam(args):
-    Session = connect_database()
-    sample_id = get_sample_id(args.bam)
-    flowcell_id = get_flowcelid_bam(args.bam)
-    with Session() as session:
-        if not session.query(Sample).filter(Sample.sample == sample_id).filter(Sample.flowcell == flowcell_id).all():
-            sample = Sample(sample=sample_id, flowcell=flowcell_id, refset=args.refset)
-            session.add(sample)
-            session.commit()
-        refset_db = (
-            session.query(Sample).filter(Sample.sample == sample_id).filter(Sample.flowcell == flowcell_id).one().refset
-        )
-        if args.print_refset_stdout:
-            print(refset_db)
-        return refset_db
+    database.functions.add_sample_to_db_and_return_refset_bam(args.bam, args.refset, args.print_refset_stdout)
 
-
-def get_sample_id(bam):
-    with pysam.AlignmentFile(bam, "rb") as workfile:
-        sampleid = []
-        for readgroup in workfile.header['RG']:
-            sampleid.append(readgroup['SM'])
-        sampleid = list(set(sampleid))
-        sampleid = "_".join(sampleid)
-    return sampleid
-
+def print_all_samples(args):
+    database.functions.print_all_samples() 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
