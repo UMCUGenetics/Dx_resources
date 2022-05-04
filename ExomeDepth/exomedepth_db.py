98ef74340216a310d78dff06ce1b8e6ab01ea58f
melferink <m.elferink@umcutrecht.nl>
2022-05-04 12:40:25 +0200

    included print commands + refactor

--- ExomeDepth/exomedepth_db.py
+++ ExomeDepth/exomedepth_db.py
@@ -6,32 +6,91 @@ import database.functions
 import settings
 
 
+def print_message(method, sample_id=None, flowcell_id=None, refset=None):
+    if method == "not_detected_in_db":
+        print("## Sample {0} with flowcell_id {1} not in refset database".format(sample_id, flowcell_id))
+    if method == "added_to_db":
+        print("## Sample {0} with flowcell_id {1} and with refset {2} added to database".format(
+            sample_id, flowcell_id, refset
+        ))
+    if method == "present_in_db":
+        print("## Sample {0} with flowcell_id {1} and with refset {2} already in database".format(
+            sample_id, flowcell_id, refset
+        ))
+    if method == "updated_db":
+        print("## Changed refset of sample {0} with flowcell_id {1} to refset {2}".format(
+           sample_id, flowcell_id, refset
+        ))
+    if method == "deleted_in_db":
+        print("## Deleted sample {0} with flowcell_id {1}.".format(sample_id, flowcell_id))
+
+
+def print_added(flowcell_id, sample_id, refset, added):
+    if added:
+        print_message("added_to_db", sample_id=sample_id, flowcell_id=flowcell_id, refset=refset)
+    else:
+        print_message("present_in_db", sample_id=sample_id, flowcell_id=flowcell_id, refset=refset)
+
+
 def call_add_sample_to_db(args):
-    database.functions.add_sample_to_db(args.flowcell_id, args.sample_id, args.refset, args.print_message)
-
-
-def call_change_refset_in_db(args):
-    database.functions.change_refset_in_db(args.flowcell_id, args.sample_id, args.refset)
-
-
-def call_query_refset(args):
-    database.functions.query_refset(args.flowcell_id, args.sample_id)
-
-
-def call_query_refset_bam(args):
-    database.functions.query_refset_bam(args.bam)
-
-
-def call_delete_sample_db(args):
-    database.functions.delete_sample_db(args.flowcell_id, args.sample_id)
+    flowcell_id, sample_id, refset_db, added = database.functions.add_sample_to_db(
+        args.flowcell_id, args.sample_id, args.refset
+    )
+    if args.print_message:
+        print_added(flowcell_id, sample_id, refset_db, added)
+    if args.print_refset_stdout:
+        print(refset_db)
 
 
 def call_add_sample_to_db_and_return_refset_bam(args):
-    database.functions.add_sample_to_db_and_return_refset_bam(args.bam, args.refset, args.print_refset_stdout)
+    flowcell_id, sample_id, refset_db, added = database.functions.add_sample_to_db_and_return_refset_bam(
+        args.bam, args.refset
+    )
+    if args.print_message:
+        print_added(flowcell_id, sample_id, refset_db, added)
+    if args.print_refset_stdout:
+        print(refset_db)
+
+
+def call_change_refset_in_db(args):
+    flowcell_id, sample_id, refset, updated = database.functions.change_refset_in_db(
+        args.flowcell_id, args.sample_id, args.refset
+    )
+    if updated:
+        print_message("updated_db", sample_id=sample_id, flowcell_id=flowcell_id, refset=refset)
+    else:
+        print_message("not_detected_in_db", sample_id=sample_id, flowcell_id=flowcell_id)
+
+
+def call_query_refset(args):
+    refset_db, flowcell_id = database.functions.query_refset(args.flowcell_id, args.sample_id)
+    if refset_db:
+        print(refset_db)
+    else:
+        print_message("not_detected_in_db", sample_id=args.sample_id, flowcell_id=flowcell_id)
+
+
+def call_query_refset_bam(args):
+    refset_db, flowcell_id, sample_id = database.functions.query_refset_bam(args.bam)
+    if refset_db:
+        print(refset_db)
+    else:
+        print_message("not_detected_in_db", sample_id=sample_id, flowcell_id=flowcell_id)
+
+
+def call_delete_sample_db(args):
+    removed, flowcell_id = database.functions.delete_sample_db(args.flowcell_id, args.sample_id)
+    if removed:
+        print_message("deleted_in_db", sample_id=args.sample_id, flowcell_id=flowcell_id)
+    else:
+        print_message("not_detected_in_db", sample_id=args.sample_id, flowcell_id=flowcell_id)
 
 
 def call_print_all_samples(args):
-    database.functions.print_all_samples()
+    sample_list = database.functions.return_all_samples()
+    print("Name\tFlowcell\tRefset\tFamilyID")
+    for line in sample_list:
+        print(line)
 
 
 if __name__ == "__main__":
@@ -46,7 +105,8 @@ if __name__ == "__main__":
         '--refset', default=settings.refset,
         help='exomedepth reference set ID [default = settings.refset]'
     )
-    parser_add.add_argument('--print_message', default=True, help='print message if added to db or not [default = True]')
+    parser_add.add_argument('--print_message', action='store_true', help='print message if added to db or not')
+    parser_add.add_argument('--print_refset_stdout', action='store_true', help='print refset in stdout')
     parser_add.set_defaults(func=call_add_sample_to_db)
 
     """ Arguments add sample to database based on BAM file and return refset"""
@@ -59,7 +119,8 @@ if __name__ == "__main__":
         '--refset', default=settings.refset,
         help='exomedepth reference set ID [default = settings.refset]'
     )
-    parser_add_return_bam.add_argument('--print_refset_stdout', default=True, help='print refset in stdout [default = True]')
+    parser_add_return_bam.add_argument('--print_message', action='store_true', help='print message if added to db or not')
+    parser_add_return_bam.add_argument('--print_refset_stdout', action='store_true', help='print refset in stdout]')
     parser_add_return_bam.set_defaults(func=call_add_sample_to_db_and_return_refset_bam)
 
     """ Arguments query refset"""
