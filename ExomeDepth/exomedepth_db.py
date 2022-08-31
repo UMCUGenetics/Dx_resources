1a11699eb55e723fdc4c94dbbe781ce65ff9dee3
melferink <m.elferink@umcutrecht.nl>
2022-08-31 11:36:36 +0200

    removed call_ addition functions

--- ExomeDepth/exomedepth_db.py
+++ ExomeDepth/exomedepth_db.py
@@ -32,7 +32,7 @@ def print_added(flowcell_id, sample_id, refset, added):
         print_message("present_in_db", sample_id=sample_id, flowcell_id=flowcell_id, refset=refset)
 
 
-def call_add_sample_to_db(args):
+def add_sample_to_db(args):
     flowcell_id, sample_id, refset_db, added = database.functions.add_sample_to_db(
         args.flowcell_id, args.sample_id, args.refset
     )
@@ -42,7 +42,7 @@ def call_add_sample_to_db(args):
         print(refset_db)
 
 
-def call_add_sample_to_db_and_return_refset_bam(args):
+def add_sample_to_db_and_return_refset_bam(args):
     flowcell_id, sample_id, refset_db, added = database.functions.add_sample_to_db_and_return_refset_bam(
         args.bam, args.refset
     )
@@ -52,7 +52,7 @@ def call_add_sample_to_db_and_return_refset_bam(args):
         print(refset_db)
 
 
-def call_change_refset_in_db(args):
+def change_refset_in_db(args):
     flowcell_id, sample_id, refset, updated = database.functions.change_refset_in_db(
         args.flowcell_id, args.sample_id, args.refset
     )
@@ -62,7 +62,7 @@ def call_change_refset_in_db(args):
         print_message("not_detected_in_db", sample_id=sample_id, flowcell_id=flowcell_id)
 
 
-def call_query_refset(args):
+def query_refset(args):
     refset_db, flowcell_id = database.functions.query_refset(args.flowcell_id, args.sample_id)
     if refset_db:
         print(refset_db)
@@ -70,7 +70,7 @@ def call_query_refset(args):
         print_message("not_detected_in_db", sample_id=args.sample_id, flowcell_id=flowcell_id)
 
 
-def call_query_refset_bam(args):
+def query_refset_bam(args):
     refset_db, flowcell_id, sample_id = database.functions.query_refset_bam(args.bam)
     if refset_db:
         print(refset_db)
@@ -78,7 +78,7 @@ def call_query_refset_bam(args):
         print_message("not_detected_in_db", sample_id=sample_id, flowcell_id=flowcell_id)
 
 
-def call_delete_sample_db(args):
+def delete_sample_db(args):
     removed, flowcell_id = database.functions.delete_sample_db(args.flowcell_id, args.sample_id)
     if removed:
         print_message("deleted_in_db", sample_id=args.sample_id, flowcell_id=flowcell_id)
@@ -86,14 +86,14 @@ def call_delete_sample_db(args):
         print_message("not_detected_in_db", sample_id=args.sample_id, flowcell_id=flowcell_id)
 
 
-def call_print_all_samples(args):
+def print_all_samples(args):
     sample_list = database.functions.return_all_samples()
     print("Name\tFlowcell\tRefset")
     for line in sample_list:
         print(line)
 
 
-def call_fill_database(args):
+def fill_database(args):
     folders = database.functions.get_folder_sorted(args.path)
     conflicts = {"warning": {}, "present": {}}
     for folder in folders:
@@ -124,7 +124,7 @@ if __name__ == "__main__":
     )
     parser_add.add_argument('--print_message_stdout', action='store_true', help='print message if added to db or not')
     parser_add.add_argument('--print_refset_stdout', action='store_true', help='print refset in stdout')
-    parser_add.set_defaults(func=call_add_sample_to_db)
+    parser_add.set_defaults(func=add_sample_to_db)
 
     """ Arguments add sample to database based on BAM file and return refset"""
     parser_add_return_bam = subparser.add_parser(
@@ -140,7 +140,7 @@ if __name__ == "__main__":
         '--print_message_stdout', action='store_true', help='print message if added to db or not'
     )
     parser_add_return_bam.add_argument('--print_refset_stdout', action='store_true', help='print refset in stdout]')
-    parser_add_return_bam.set_defaults(func=call_add_sample_to_db_and_return_refset_bam)
+    parser_add_return_bam.set_defaults(func=add_sample_to_db_and_return_refset_bam)
 
     """ Arguments query refset"""
     parser_query_refset = subparser.add_parser(
@@ -149,7 +149,7 @@ if __name__ == "__main__":
     )
     parser_query_refset.add_argument('sample_id', help='sample id')
     parser_query_refset.add_argument('flowcell_id', nargs='+', help='flowcell barcode')
-    parser_query_refset.set_defaults(func=call_query_refset)
+    parser_query_refset.set_defaults(func=query_refset)
 
     """ Arguments query refset based on BAM file"""
     parser_query_refset_bam = subparser.add_parser(
@@ -157,7 +157,7 @@ if __name__ == "__main__":
         help='return refset of sample based on BAM file'
     )
     parser_query_refset_bam.add_argument('bam', help='full path to BAM file')
-    parser_query_refset_bam.set_defaults(func=call_query_refset_bam)
+    parser_query_refset_bam.set_defaults(func=query_refset_bam)
 
     """ Arguments change refset in database for sample"""
     parser_change = subparser.add_parser(
@@ -166,14 +166,14 @@ if __name__ == "__main__":
     parser_change.add_argument('sample_id', help='sample id')
     parser_change.add_argument('flowcell_id', help='flowcell barcode (as in database)')
     parser_change.add_argument('refset', help='new refset ID')
-    parser_change.set_defaults(func=call_change_refset_in_db)
+    parser_change.set_defaults(func=change_refset_in_db)
 
     """ Arguments to print all entries of database """
     parser_allsamples = subparser.add_parser(
         'print_all_samples',
         help='Print full database table (tab-separated)'
     )
-    parser_allsamples.set_defaults(func=call_print_all_samples)
+    parser_allsamples.set_defaults(func=print_all_samples)
 
     """ Arguments delete sample in database"""
     parser_delete_sample = subparser.add_parser(
@@ -182,7 +182,7 @@ if __name__ == "__main__":
     )
     parser_delete_sample.add_argument('sample_id', help='sample id')
     parser_delete_sample.add_argument('flowcell_id', nargs='+', help='flowcell barcode')
-    parser_delete_sample.set_defaults(func=call_delete_sample_db)
+    parser_delete_sample.set_defaults(func=delete_sample_db)
 
     """ Arguments fill database based on cohort sampels"""
     parser_fill_database = subparser.add_parser(
@@ -195,7 +195,7 @@ if __name__ == "__main__":
         default="conflicts.txt",
         help='output file name containing conflicts that must be resolved manually (default = conflicts.txt)'
     )
-    parser_fill_database.set_defaults(func=call_fill_database)
+    parser_fill_database.set_defaults(func=fill_database)
 
     args = parser.parse_args()
     args.func(args)
