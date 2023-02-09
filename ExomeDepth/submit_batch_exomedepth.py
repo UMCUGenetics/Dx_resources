53fb3868bef07829891997e7ba2fd64dd8fa53ef
melferink <m.elferink@umcutrecht.nl>
2023-02-09 09:47:24 +0100

    refactor to fix reanalysis bug family IGV session

--- ExomeDepth/submit_batch_exomedepth.py
+++ ExomeDepth/submit_batch_exomedepth.py
@@ -17,6 +17,7 @@ def exomedepth_analysis(bam, args, gender_dic, suffix_dic, refset_dic):
     bamfile = bam.split("/")[-1]
     sampleid = database.functions.get_sample_id(bam)
     bam_path = os.path.abspath(bam)
+    run = args.inputfolder.rstrip("/").split("/")[-1]
 
     if args.reanalysis and sampleid in refset_dic:  # Use refset in reanalysis argument file if provided in argument
         refset = refset_dic[sampleid]
@@ -42,7 +43,7 @@ def exomedepth_analysis(bam, args, gender_dic, suffix_dic, refset_dic):
             exomedepth=args.exomedepth,
             output=args.outputfolder,
             inputbam=bamfile,
-            run=args.inputfolder.rstrip("/").split("/")[-1],
+            run=run,
             sample=sampleid,
             refset=refset,
             length=args.expectedCNVlength,
@@ -72,31 +73,28 @@ def exomedepth_analysis(bam, args, gender_dic, suffix_dic, refset_dic):
     os.system("mv {output}/{sampleid}/UMCU*.vcf {output}/UMCU/".format(sampleid=sampleid, output=args.outputfolder))
     os.system("rm -r {output}/{sample}".format(output=args.outputfolder, sample=sampleid))
 
-    return [sampleid, bamfile, refset]
+    hc_cnv_vcf = "{output}/HC/HC_{refset}_{sampleid}_{run}_exome_calls".format(
+        output=args.outputfolder,
+        refset=refset,
+        sampleid=sampleid,
+        run=run,
+    )
 
+    ed_igv = "{output}/igv_tracks/HC_{refset}_{sampleid}_{run}".format(
+        output=args.outputfolder, 
+        refset=refset,
+        sampleid=sampleid,
+        run=run
+    )
 
-def parse_ped(ped_file):
-    samples = {}  # 'sample_id': {'family': 'fam_id', 'parents': ['sample_id', 'sample_id']}
-    for line in ped_file:
-        ped_data = line.strip().split()
-        family, sample, father, mother, sex, phenotype = ped_data
+    if sampleid in suffix_dic:
+        hc_cnv_vcf = "{}_{}.vcf".format(hc_cnv_vcf, suffix_dic[sampleid])
+        ed_igv = "{}_{}_ref.igv".format(ed_igv, suffix_dic[sampleid]) 
+    else:  
+        hc_cnv_vcf = "{}.vcf".format(hc_cnv_vcf)
+        ed_igv = "{}_ref.igv".format(ed_igv)
 
-        # Create samples
-        if sample not in samples:
-            samples[sample] = {'family': family, 'parents': [], 'children': []}
-        if father != '0' and father not in samples:
-            samples[father] = {'family': family, 'parents': [], 'children': []}
-        if mother != '0' and mother not in samples:
-            samples[mother] = {'family': family, 'parents': [], 'children': []}
-
-        # Save sample relations
-        if father != '0':
-            samples[sample]['parents'].append(father)
-            samples[father]['children'].append(sample)
-        if mother != '0':
-            samples[sample]['parents'].append(mother)
-            samples[mother]['children'].append(sample)
-    return samples
+    return [sampleid, bamfile, refset, hc_cnv_vcf, ed_igv]
 
 
 if __name__ == "__main__":
@@ -139,6 +137,7 @@ if __name__ == "__main__":
         )
     elif args.pipeline == "nf":
         bams = glob.glob("{}/bam_files/**/*.bam".format(args.inputfolder), recursive=True)
+
     print("Number of BAM files = "+str(len(bams)))
 
     if bams:
@@ -147,6 +146,25 @@ if __name__ == "__main__":
         )
     else:
         sys.exit("no bam files detected")
+
+    snv_vcf_files = []
+    for snv_vcf_file in glob.glob("{}/single_sample_vcf/*.vcf".format(args.inputfolder), recursive=True):
+        snv_vcf_files.append(os.path.basename(snv_vcf_file))
+
+    baf_files = []
+    for baf_file in glob.glob("{}/baf/*.igv".format(args.inputfolder), recursive=True):
+        baf_files.append(os.path.basename(baf_file))
+
+    upd_files =[]
+    for upd_file in glob.glob("{}/upd/*.igv".format(args.inputfolder), recursive=True):
+        upd_files.append(os.path.basename(upd_file))
+
+    if any(len(lst) != len(bams) for lst in [snv_vcf_files, baf_files]):
+        sys.exit((
+            "unequal number of files found: bams={0}, snvVCF={1}, baf={2}"
+        ).format(
+            len(bams), len(snv_vcf_files), len(baf_files)
+        ))
 
     """ Check if previous exomedepth analysis is already present """
     if os.path.isdir(args.outputfolder):
@@ -195,7 +213,6 @@ if __name__ == "__main__":
     refset_dic = {}
     gender_dic = {}
     suffix_dic = {}
-    metadata_dic = {}
     if args.reanalysis:  # Use predetermined refset for each sample based on the reanalysis option input
         with open(args.reanalysis) as refset_file:
             for line in refset_file:
@@ -239,16 +256,38 @@ if __name__ == "__main__":
             settings.igv_xml, args.outputfolder, item[0], args.runid, item[2], item[1]
         )
         if item[0] in suffix_dic:
-            action = "{action} --vcf_filename_suffix {suffix}".format(action=action, suffix=suffix_dic[item[0]])
+            action = "{action} --reanalysis {reanalysis}".format(action=action, reanalysis=args.reanalysis)
         os.system(action)
 
     """ Make family IGV session(s)"""
-    families = parse_ped(open(args.pedfile, "r"))
+    cnv_vcf_files = []
+    igv_files = []
     for item in sampleinfo:
-        if len(list(set(families[item[0]]["parents"]))) == 2:
-            action = "python {0} family_igv {1} {2} {3} {4} {5}".format(
-                settings.igv_xml, args.outputfolder, args.pedfile, args.runid, item[0], " ".join(bams)
-            )
-            if item[0] in suffix_dic:
-                action = "{action} --vcf_filename_suffix {suffix}".format(action=action, suffix=suffix_dic[item[0]])
-            os.system(action)
+        cnv_vcf_files.append(os.path.basename(item[3]))
+        igv_files.append(os.path.basename(item[4]))
+
+    bam_files = [] 
+    for bam in bams:
+        bam_files.append(os.path.basename(bam))
+
+    action = (
+        "python {0} family_igv {1} {2} {3} --bam_files {4} "
+        "--snv_vcf_files {5} --cnv_vcf_files {6} --igv_files {7} "
+        "--upd_files {8} --baf_files {9}"
+    ).format(
+        settings.igv_xml,
+        args.outputfolder,
+        args.pedfile,
+        args.runid,
+        " ".join(bam_files),
+        " ".join(snv_vcf_files),
+        " ".join(cnv_vcf_files),
+        " ".join(igv_files),
+        " ".join(upd_files),
+        " ".join(baf_files)
+    )
+ 
+    if args.reanalysis:
+        "{action} --reanalysis {reanalysis}".format(action=action, reanalysis=args.reanalysis)
+
+    os.system(action)
