c92ab59a8d5e105108f5d9d8080e3c6ecd47154e
melferink <m.elferink@umcutrecht.nl>
2025-08-06 12:37:45 +0200

    changed order fields to make identical to legacy script changes

--- ExomeDepth/ed_csv_to_vcf.py
+++ ExomeDepth/ed_csv_to_vcf.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     new_record.samples[0].data = collections.namedtuple('CallData', format_keys)  # For single sample VCF only!
     format_vals = [record.samples[0].data[vx] for vx in range(len(format_keys))]
     format_dict = dict(zip(format_keys, format_vals))
-    for f in ['GT', 'CN', 'BF', 'BC', 'SM', 'CR', 'RS', 'IH', 'CM', 'PD', 'TC']:
+    for f in ['GT', 'CN', 'BF', 'SM', 'CR', 'RS', 'IH', 'CM', 'PD', 'TC', 'BC']:
         format_dict[f] = ""
     new_vals = [format_dict[x] for x in format_keys]
     new_record.samples[0].data = new_record.samples[0].data._make(new_vals)
@@ -200,12 +200,11 @@ if __name__ == "__main__":
             new_record.INFO['cCNV'] = call_conrad
 
             """Change FORMAT fields"""
-            for f in ['GT', 'CN', 'BF', 'BC', 'SM', 'CR', 'RS', 'IH', 'CM', 'PD', 'TC']:
+            for f in ['GT', 'CN', 'BF', 'SM', 'CR', 'RS', 'IH', 'CM', 'PD', 'TC', 'BC']:
                 format_dict[f] = ""
             format_dict['GT'] = str(genotype)
             format_dict['CN'] = "%.2f" % (float(calc_copynumber))
             format_dict['BF'] = "%.2f" % (float(row['BF']))
-            format_dict['BC'] = row['nexons']
             format_dict['SM'] = "%.2f" % (float(ratio))
             format_dict['CR'] = "%.4f" % (float(row['correlation']))
             format_dict['RS'] = row['refsize']
@@ -213,6 +212,7 @@ if __name__ == "__main__":
             format_dict['CM'] = args.calling_model
             format_dict['PD'] = perc_del
             format_dict['TC'] = total_calls
+            format_dict['BC'] = row['nexons']
             new_vals = [format_dict[x] for x in format_keys]
             new_record.samples[0].data = new_record.samples[0].data._make(new_vals)  # GT must be first in order of metadata!
             vcf_writer.write_record(new_record)
